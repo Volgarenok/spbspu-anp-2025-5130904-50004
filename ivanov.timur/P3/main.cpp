ff7ab354eaffd28987a22c4ae0a9b3aafdba3f39
isaacgreenfield <ivtim708@gmail.com>
2025-11-03 11:54:54 +0300

    Added namespace ivanov

--- ivanov.timur/P3/main.cpp
+++ ivanov.timur/P3/main.cpp
@@ -1,3 +1,16 @@
-int main() {
+#include <fstream>
+namespace ivanov {
+    struct Dynamic {
+        int rows, cols;
+        int * matrix = reinterpret_cast< int * >(malloc(sizeof(int) * (rows * cols)));
+    };
+    struct Static {
+        int rows, cols;
+        int matrix[1000] = {};
+    };
+
+}
+int main(int argc, char ** argv)
+{
 
 }
