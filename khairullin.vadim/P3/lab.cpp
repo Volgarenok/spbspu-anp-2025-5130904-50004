88d58267786b9464cd19f28bea4e6f2c3c60112b
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-05 19:22:34 +0300

    I made the arrays

---
+++ khairullin.vadim/P3/lab.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+int main (int argc, char ** argv)
+{
+  if (argv[1] == 1)
+  {
+    int array[10000] = {};
+  }
+  else if (argv[1] == 2)
+  {
+    int * array = reinterpret_cast<int*>(rows * cols * sizeof(int));
+  }
+}
