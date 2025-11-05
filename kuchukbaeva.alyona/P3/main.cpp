4c29bab378833b2f299f715b20aa38c3d2f8c26f
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-05 20:10:49 +0300

    Check errors dlia vvoda, vse i ustala, dalshe nado dumat

--- kuchukbaeva.alyona/P3/main.cpp
+++ kuchukbaeva.alyona/P3/main.cpp
@@ -110,6 +110,31 @@ namespace kuchukbaeva
     return true;
   }
 }
-int main()
+int main(int argc,  char* argv[])
 {
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments" << "\n";
+    return 1;
+  }
+    if (argc < 4)
+  {
+    std::cerr << "Not enough arguments" << "\n";
+    return 1;
+  }
+  int num;
+  try
+  {
+    num = std::stoi(argv[1]);
+  }
+  catch (...)
+  {
+    std::cerr << "First parameter is not a number" << "\n";
+    return 1;
+  }
+  if (num != 1 && num != 2)
+  {
+    std::cerr << "First parameter is out of range" << "\n";
+    return 1;
+  }
 }
