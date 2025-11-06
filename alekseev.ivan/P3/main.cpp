2a07a7603ffa0d7aa153868b79c40b483fe92655
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-06 21:29:20 +0300

    test file input output

--- alekseev.ivan/P3/main.cpp
+++ alekseev.ivan/P3/main.cpp
@@ -4,6 +4,20 @@
 int main(int argc, char ** argv)
 {
   std::ifstream input(argv[2]);
-  std::ofstream output(argv[3]);
+  size_t m = 0ull, n = 0ull;
+  input >> m >> n;
+  int matrix[10000];
+  for (size_t i = 0ull; i < m; ++i) {
+    for (size_t j = 0ull; j < n; ++j) {
+      input >> matrix[i * n + j];
+    }
+  }
 
+  std::ofstream output(argv[3]);
+  for (size_t i = 0ull; i < m; ++i) {
+    for (size_t j = 0ull; j < n; ++j) {
+      output << matrix[i * n + j] << " ";
+    }
+    output << "\n";
+  }
 }
