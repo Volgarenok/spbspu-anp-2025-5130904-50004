dcfdb68439e228084042f1e7c4ef99846b46fd4c
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-11-06 22:11:30 +0300

    check command line arguments

--- alekseev.ivan/P3/main.cpp
+++ alekseev.ivan/P3/main.cpp
@@ -1,23 +1,40 @@
 #include <iostream>
 #include <fstream>
 
+namespace alekseev {
+  bool check_cl_args(int argc, char *argv[]);
+}
+
 int main(int argc, char ** argv)
 {
-  std::ifstream input(argv[2]);
-  size_t m = 0ull, n = 0ull;
-  input >> m >> n;
-  int matrix[10000];
-  for (size_t i = 0ull; i < m; ++i) {
-    for (size_t j = 0ull; j < n; ++j) {
-      input >> matrix[i * n + j];
-    }
+  try {
+    alekseev::check_cl_args(argc, argv);
+  } catch (const std::exception & e) {
+    std::cerr << e.what() << "\n";
+    return 1;
   }
 
-  std::ofstream output(argv[3]);
-  for (size_t i = 0ull; i < m; ++i) {
-    for (size_t j = 0ull; j < n; ++j) {
-      output << matrix[i * n + j] << " ";
-    }
-    output << "\n";
+  char num = argv[1][0];
+  std::cout << num << "\n";
+
+}
+
+
+bool alekseev::check_cl_args(int argc, char * argv[])
+{
+  if (argc < 4) {
+    throw std::invalid_argument("Not enough arguments");
+  } else if (argc > 4) {
+    throw std::invalid_argument("Too many arguments");
   }
+  for (char * i = argv[1]; *i != '\0'; ++i) {
+    if (*i < '0' or *i > '9') {
+      throw std::invalid_argument("First parameter is not a number");
+    }
+  }
+  char num = argv[1][0];
+  if (!(num == '1' || num == '2')) {
+    throw std::invalid_argument("First parameter is out of range");
+  }
+  return true;
 }
