2c15041d5ac10513db5de75fc46961b390e9d4a6
knopkloop <hruslan2007@gmail.com>
2025-11-08 20:23:10 +0300

    Skeleton for Programm (try 4)

--- haliullin.ruslan/P3/main.cpp
+++ haliullin.ruslan/P3/main.cpp
@@ -3,33 +3,28 @@
 
 namespace haliullin
 {
-  std::istream& input(std::istream& in, int *arr, size_t rows, size_t cols);
-  std::ostream& output(std::ostream& out, const int *arr, size_t rows, size_t cols);
-  void FllIncrementWave(int *arr, size_t rows, size_t cols, int res);
-  int NumColLsr(int *arr, size_t rows, size_t cols);
+std::istream& input(std::istream& in, int *arr, size_t rows, size_t cols);
+std::ostream& output(std::ostream& out, const int *arr, size_t rows, size_t cols);
+void FllIncrementWave(int *arr, size_t rows, size_t cols, int res);
+int NumColLsr(int *arr, size_t rows, size_t cols);
 }
 
 int main(int argc, char **argv)
 {
-
 }
 
 std::istream& haliullin::input(std::istream& in, int *arr, size_t rows, size_t cols)
 {
-
 }
 
 std::ostream& haliullin::output(std::ostream& out, const int *arr, size_t rows, size_t cols)
 {
-
 }
 
 void haliullin::FllIncrementWave(int *arr, size_t rows, size_t cols, int res)
 {
-
 }
 
 int haliullin::NumColLsr(int *arr, size_t rows, size_t cols)
 {
-
 }
