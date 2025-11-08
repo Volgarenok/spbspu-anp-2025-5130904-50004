11d71bc68847dffcd78d1be89ef49286da69d2ef
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-08 14:43:35 +0300

    I added the function for checking command line's arguments and opened file for outputing

--- khairullin.vadim/P3/lab.cpp
+++ khairullin.vadim/P3/lab.cpp
@@ -6,13 +6,34 @@ void filling(int * array, std::ifstream &
 {
   for (size_t i = 0; i < rows * cols; ++i)
   {
-    input >> array[i];
-    if (std::cin.fail())
+    if (std::cin.fail() or std::cin.eof())
     {
       throw std::invalid_argument("Error:
         file includes the mistake");
     }
+    input >> array[i];
   }
+}
+
+int check_arguments(int argc, char ** argv)
+{
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments";
+  }
+  else if (argc > 4)
+  {
+    std::cerr << "Too many arguments";
+  }
+  else if (argv[1][0] > 2 or argv[1][0] < 1)
+  {
+    std::cerr << "First parameter is out of range";
+  }
+  else
+  {
+    std::cerr << "First parameter is not a number";
+  }
+  return 1;
 }
 
 void work_with_static(int rows, int cols,
@@ -26,7 +47,8 @@ void worl_with_dinamic(int rows, int cols,
   std::ifstream & input)
 {
   int * array = reinterpret_cast<int *> 
-    (std::malloc(rows * cols * sizeof(int)));
+    (std::malloc(rows * cols *
+      sizeof(int)));
   if (array == nullptr)
   {
     throw std::bad_alloc();
@@ -38,18 +60,28 @@ int main (int argc, char ** argv)
 {
   int rows = 0, cols = 0;
   std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
   input >> rows >> cols;
-  int * array = nullptr;
-  if (argv[1][0] == 1)
+  check_arguments(argc, argv);
+  if (rows != 0 and cols != 0)
   {
-    work_with_static(rows, cols, input);
+    if (argv[1][0] == 1)
+    {
+      work_with_static(rows, cols, input);
+    }
+    else if (argv[1][0] == 2)
+    {
+      work_with_dinamic(rows, cols, input);
+    }
   }
-  else if (argv[1][0] == 2)
+  else if ((rows == 0 and cols != 0) or
+    (rows != 0 and cols == 0))
   {
-    work_with_dinamic(rows, cols, input);
+    throw std::runtime_error("Invalid rows or columns");
   }
-  else
+  else if (rows == 0 and cols == 0)
   {
-    throw std::runtime_error("Unknown command\n");
+    output << "Count of local maximums - 0\n";
+    output << "False";
   }
 }
