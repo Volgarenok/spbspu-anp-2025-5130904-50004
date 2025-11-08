79b3dffeb63ca4c580853ee6330bea3c8eabfe0b
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-09 02:14:53 +0300

    Add main

--- ivantsova.ekaterina/P3/main.cpp
+++ ivantsova.ekaterina/P3/main.cpp
@@ -123,6 +123,97 @@ int ivantsova::countColumns(int * arr, size_t rows, size_t cols)
   return count;
 }
 
-int main()
+int main(int argc, char ** argv)
 {
+  if (argc > 4)
+  {
+    std::cerr << "Too many arguments" << "\n";
+    return 1;
+  }
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments" <<"\n";
+    return 1;
+  }
+  if (argv[1][0] != '1' && argv[1][0] != '2')
+  {
+    std::cerr << "First parameter is out of range or first parameter is not a number" << "\n";
+    return 1;
+  }
+  std::ifstream input(argv[2]);
+  std::ofstream output(argv[3]);
+  if (!input.is_open() || !output.is_open())
+  {
+    std::cerr << "Error opening files" << "\n";
+    return 1;
+  }
+  size_t rows = 0;
+  size_t cols = 0;
+  int result = 0;
+  if (!(input >> rows))
+  {
+    std::cerr << "Incorrect number of rows" << "\n";
+    return 2;
+  }
+  else if (!(input >>cols))
+  {
+    std::cerr << "Incorrect number of columns" << "\n";
+    return 2;
+  }
+  if (argv[1][0] == '1')
+  {
+    if (rows * cols > 10000)
+    {
+      std::cerr << "Too large matrix" << "\n";
+      return 2;
+    }
+    int arr[10000] = {};
+    ivantsova::readArr(input, arr, rows, cols);
+    if (!input)
+    {
+      return 2;
+    }
+    input.close();
+    int arr_copy[10000];
+    for (size_t i = 0; i < rows * cols; i++)
+    {
+      arr_copy[i] = arr[i];
+    }
+    result = ivantsova::countColumns(arr_copy, rows, cols);
+    ivantsova::reduceElements(arr, rows, cols);
+    ivantsova::writeArr(output, arr, rows, cols, result);
+  }
+  else if (argv[1][0] == '2')
+  {
+    int * arr = nullptr;
+    int * arr_copy = nullptr;
+    try
+    {
+      arr = new int [rows * cols] {};
+      ivantsova::readArr(input, arr, rows, cols);
+      if (!input)
+      {
+        delete[] arr;
+        return 2;
+      }
+      input.close();
+      arr_copy = new int [rows * cols]();
+      for (size_t i = 0; i < rows * cols; i++)
+      {
+        arr_copy[i] = arr[i];
+      }
+      result = ivantsova::countColumns(arr_copy, rows, cols);
+      ivantsova::reduceElements(arr, rows, cols);
+      ivantsova::writeArr(output, arr, rows, cols, result);
+      delete[] arr_copy;
+      delete[] arr;
+    }
+    catch (...)
+    {
+      std::cerr << "Error" << "\n";
+      delete[] arr;
+      return 1;
+    }
+  }
+  return 0;
 }
