3287aea794feab206be294eaa9b5b6ee4271d2b4
knopkloop <hruslan2007@gmail.com>
2025-11-08 19:47:22 +0300

    Skeleton for programm

--- haliullin.ruslan/P3/main.cpp
+++ haliullin.ruslan/P3/main.cpp
@@ -1,5 +1,36 @@
 #include <iostream>
-int main()
+#include <fstream>
+
+namespace haliullin
 {
+  std::istream& input(std::istream& in, int* arr, size_t rows, size_t cols);
+  std::ostream& output(std::ostream& out, const int* arr, size_t rows, size_t cols);
+  void FllIncrementWave(int *arr, size_t rows, size_t cols, int res);
+  int NumColLsr(int *arr, size_t rows, size_t cols);
 }
 
+
+int main(int argc, char ** argv)
+{
+  
+}
+
+std::istream& input(std::istream& in, int *arr, size_t rows, size_t cols)
+{
+
+}
+
+std::ostream& output(std::ostream& out, const int* arr, size_t rows, size_t cols)
+{
+
+}
+
+void FllIncrementWave(int *arr, size_t rows, size_t cols, int res)
+{
+
+}
+
+int NumColLsr(int *arr, size_t rows, size_t cols)
+{
+
+}
