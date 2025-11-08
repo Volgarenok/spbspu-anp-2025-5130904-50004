2c6c319dd60c28befd0245402fd460ce336bf04f
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-09 00:54:18 +0300

    Program structure

--- ivantsova.ekaterina/P3/main.cpp
+++ ivantsova.ekaterina/P3/main.cpp
@@ -1,3 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+namespace ivantsova
+{
+  std::istream & readArr(std::istream & input, int * arr, size_t rows, size_t cols);
+  std::ostream & writeArr(std::ostream & output, const int * arr, size_t rows, size_t cols, int result);
+  void reduceElements(int * arr, size_t rows, size_t cols);
+  int countColumns(int * arr, size_t rows, size_t cols);
+}
+
+std::istream & ivantsova::readArr(std::istream & input, int * arr, size_t rows, size_t cols)
+{
+}
+
+std::ostream & ivantsova::writeArr(std::ostream & output, const int * arr, size_t rows, size_t cols, int result)
+{
+}
+
+void ivantsova::reduceElements(int * arr, size_t rows, size_t cols)
+{
+}
+
+int ivantsova::countColumns(int * arr, size_t rows, size_t cols)
+{
+}
+
 int main()
 {
 }
