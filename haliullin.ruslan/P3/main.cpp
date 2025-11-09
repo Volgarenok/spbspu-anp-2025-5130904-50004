d25c594bb73b0faa6acc854c5a6f196762db6ec4
knopkloop <hruslan2007@gmail.com>
2025-11-09 20:21:54 +0300

    added input function

--- haliullin.ruslan/P3/main.cpp
+++ haliullin.ruslan/P3/main.cpp
@@ -4,8 +4,9 @@
 namespace haliullin
 {
   std::istream& input(std::istream& in, int *arr, size_t rows, size_t cols);
-  std::ostream& output(std::ostream& out, const int *arr, size_t rows, size_t cols);
-  void FllIncrementWave(int *arr, size_t rows, size_t cols, int res);
+  std::ostream& output(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res);
+  void FllIncrementWave(int *arr, size_t rows, size_t cols);
+  void cutToSquare(int *arr, size_t rows, size_t cols);
   int NumColLsr(int *arr, size_t rows, size_t cols);
 }
 
@@ -15,17 +16,36 @@ int main(int argc, char **argv)
 
 std::istream& haliullin::input(std::istream& in, int *arr, size_t rows, size_t cols)
 {
+  size_t count = 0;
+  for (size_t i = 0; i < rows*cols && in >> arr[i]; ++i)
+  {
+    ++count;
+  }
+  if (!in)
+  {
+    std::cerr << "Unexpected input\n";
+  }
+  else if (count < rows * cols)
+  {
+    std::cerr << "Not enough elements\n";
+  }
+  return in;
 }
 
-std::ostream& haliullin::output(std::ostream& out, const int *arr, size_t rows, size_t cols)
+std::ostream& haliullin::output(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res)
 {
 }
 
-void haliullin::FllIncrementWave(int *arr, size_t rows, size_t cols, int res)
+void haliullin::FllIncrementWave(int *arr, size_t rows, size_t cols)
+{
+}
+
+void haliullin::cutToSquare(int *arr, size_t rows, size_t cols)
 {
 }
 
 int haliullin::NumColLsr(int *arr, size_t rows, size_t cols)
 {
 }
+
 
