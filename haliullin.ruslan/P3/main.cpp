f9c4547ae742521884f4358bb567e8bd87c01fb6
knopkloop <hruslan2007@gmail.com>
2025-11-09 22:57:50 +0300

    added variant for fixed-length array in main

--- haliullin.ruslan/P3/main.cpp
+++ haliullin.ruslan/P3/main.cpp
@@ -2,8 +2,8 @@
 #include <fstream>
 namespace haliullin
 {
-  std::istream& input(std::istream& in, int *arr, size_t rows, size_t cols);
-  std::ostream& output(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res);
+  std::istream& readMatrix(std::istream& in, int *arr, size_t rows, size_t cols);
+  std::ostream& writeMatrix(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res);
   void FllIncrementWave(int *arr, size_t rows, size_t cols);
   size_t NumColLsr(int *arr, size_t rows, size_t cols);
 }
@@ -21,35 +21,79 @@ int main(int argc, char **argv)
       return 1;
     }
 
-  if (argv[1][0] != '1' && argv[1][0] != '2')
+  int temp = 0;
+  while (argv[1][temp] != '\0')
   {
-    if (!( argv[1][0] >= '0' && argv[1][0] <= '9'))
+    if (!(argv[1][0] >= '0' && argv[1][0] <= '9'))
     {
       std::cerr << "First parameter is not a number" << "\n";
       return 1;
     }
-    else
-    {
+    ++temp;
+  }
+
+  if (temp > 1)
+  {
       std::cerr << "First parameter is out of range" << "\n";
       return 1;
-    }
   }
 
   std::ifstream in(argv[2]);
   std::ofstream out(argv[3]);
   namespace hal = haliullin;
+
+  size_t rows = 0;
+  size_t cols = 0;
+  size_t res = 0;
+  if (!in.is_open() || !out.is_open())
+  {
+    std::cerr << "Opening files error" << "\n";
+    return 2;
+  }
+  if (!(in >> rows))
+  {
+    std::cerr << "Incorrect value for rows" << "\n";
+    return 2;
+  }
+  else if (!(in >> cols))
+  {
+    std::cerr << "Incorrect value for columns" << "\n";
+    return 2;
+  }
+
   if (argv[1][0] == '1')
   {
-    //
+    if (rows * cols > 10000)
+    {
+      std::cerr << "Too large size of array" << "\n";
+      return 2;
+    }
+
+    int arr[10000] = {};
+    hal::readMatrix(in, arr, rows, cols);
+
+    if (!in)
+    {
+      std::cerr << "Error of reading array" << "\n";
+      return 2;
+    }
+
+
+    in.close();
+    hal::FllIncrementWave(arr, rows, cols);
+    res = hal::NumColLsr(arr, rows, cols);
+    hal::writeMatrix(out, arr, rows, cols, res);
+
   }
+
 
   else if (argv[1][0] == '2')
   {
     //
-  }
 
+  }
 }
-std::istream& haliullin::input(std::istream& in, int *arr, size_t rows, size_t cols)
+std::istream& haliullin::readMatrix(std::istream& in, int *arr, size_t rows, size_t cols)
 {
   size_t count = 0;
   for (size_t i = 0; i < rows*cols && in >> arr[i]; ++i)
@@ -67,7 +111,7 @@ std::istream& haliullin::input(std::istream& in, int *arr, size_t rows, size_t c
   return in;
 }
 
-std::ostream& haliullin::output(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res)
+std::ostream& haliullin::writeMatrix(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res)
 {
   out << res << "\n";
   for (size_t i = 0; i < rows; ++i)
