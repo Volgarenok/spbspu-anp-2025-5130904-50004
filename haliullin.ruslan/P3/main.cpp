165471edce506befe3a6770d075cd02e1fce497c
knopkloop <hruslan2007@gmail.com>
2025-11-09 22:13:12 +0300

    added input checking in main

--- haliullin.ruslan/P3/main.cpp
+++ haliullin.ruslan/P3/main.cpp
@@ -1,19 +1,54 @@
 #include <iostream>
 #include <fstream>
-
 namespace haliullin
 {
   std::istream& input(std::istream& in, int *arr, size_t rows, size_t cols);
   std::ostream& output(std::ostream& out, const int *arr, size_t rows, size_t cols, size_t res);
   void FllIncrementWave(int *arr, size_t rows, size_t cols);
-  void cutToSquare(int *arr, size_t rows, size_t cols);
-  int NumColLsr(int *arr, size_t rows, size_t cols);
+  size_t NumColLsr(int *arr, size_t rows, size_t cols);
 }
 
 int main(int argc, char **argv)
 {
-}
+  if (argc < 4)
+  {
+    std::cerr << "Not enough arguments" << "\n";
+    return 1;
+  }
+  else if (argc > 4)
+    {
+      std::cerr << "Too many arguements" << "\n";
+      return 1;
+    }
 
+  if (argv[1][0] != '1' && argv[1][0] != '2')
+  {
+    if (!( argv[1][0] >= '0' && argv[1][0] <= '9'))
+    {
+      std::cerr << "First parameter is not a number" << "\n";
+      return 1;
+    }
+    else
+    {
+      std::cerr << "First parameter is out of range" << "\n";
+      return 1;
+    }
+  }
+
+  std::ifstream in(argv[2]);
+  std::ofstream out(argv[3]);
+  namespace hal = haliullin;
+  if (argv[1][0] == '1')
+  {
+    //
+  }
+
+  else if (argv[1][0] == '2')
+  {
+    //
+  }
+
+}
 std::istream& haliullin::input(std::istream& in, int *arr, size_t rows, size_t cols)
 {
   size_t count = 0;
@@ -23,11 +58,11 @@ std::istream& haliullin::input(std::istream& in, int *arr, size_t rows, size_t c
   }
   if (!in)
   {
-    std::cerr << "Unexpected input\n";
+    std::cerr << "Unexpected input" << "\n";
   }
   else if (count < rows * cols)
   {
-    std::cerr << "Not enough elements\n";
+    std::cerr << "Not enough elements" << "\n";
   }
   return in;
 }
@@ -50,11 +85,7 @@ void haliullin::FllIncrementWave(int *arr, size_t rows, size_t cols)
 {
 }
 
-void haliullin::cutToSquare(int *arr, size_t rows, size_t cols)
-{
-}
-
-int haliullin::NumColLsr(int *arr, size_t rows, size_t cols)
+size_t haliullin::NumColLsr(int *arr, size_t rows, size_t cols)
 {
 }
 
