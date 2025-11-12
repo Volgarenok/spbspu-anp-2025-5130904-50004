feffa161039fac4f6d7393931bb580fa93af8a0c
velizadefarida <velizadefarida@gmail.com>
2025-11-12 04:28:58 +0300

    Added namespace declaration

--- velizade.farida/P3/main.cpp
+++ velizade.farida/P3/main.cpp
@@ -1,3 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+namespace velizade
+{
+  std::istream& readArr(std::istream& input, int* arr, size_t rows, size_t cols);
+  std::ostream& writeResult(std::ostream& output, const int* arr, size_t rows, size_t cols, int result);
+  int countLocalMins(const int* arr, size_t rows, size_t cols);
+  void leftTopClockwise(int* arr, size_t rows, size_t cols);
+}
+
 int main()
 {
 }
