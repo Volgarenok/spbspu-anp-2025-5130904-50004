7b6e6698a56455229379db82524d5f2bf48f571b
velizadefarida <velizadefarida@gmail.com>
2025-11-12 04:37:21 +0300

    Added function for writing the result to a file

--- velizade.farida/P3/main.cpp
+++ velizade.farida/P3/main.cpp
@@ -23,6 +23,25 @@ std::istream& velizade::readArr(std::istream& input, int* arr, size_t rows, size
   return input;
 }
 
+std::ostream& velizade::writeResult(std::ostream& output, const int* arr, size_t rows, size_t cols, int result)
+{
+  output << result;
+  if (rows > 0 && cols > 0)
+  {
+    output << "\n";
+    for (size_t i = 0; i < rows; i++)
+    {
+      for (size_t j = 0; j < cols; j++)
+      {
+        output << arr[i * cols + j];
+        if (j < cols - 1) output << " ";
+      }
+      if (i < rows - 1) output << "\n";
+    }
+  }
+  return output;
+}
+
 int main()
 {
 }
