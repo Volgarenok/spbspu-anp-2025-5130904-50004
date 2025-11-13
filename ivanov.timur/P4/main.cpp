9fd425a91917f2dd1472d37b27e7f5e3327bc29d
isaacgreenfield <ivtim708@gmail.com>
2025-11-13 21:37:14 +0300

    Struct basics formalized

--- ivanov.timur/P4/main.cpp
+++ ivanov.timur/P4/main.cpp
@@ -23,13 +23,14 @@ namespace ivanov {
     size_t size;
     Line();
     ~Line();
-    Line & operator=(Line l);
+    Line & operator=(const Line l);
     void next(char s);
     void space_cleaner();
     void rmv(size_t id) noexcept;
 
     size_t get_size() const noexcept;
     void get_line() const noexcept;
+    char get(size_t id) const noexcept;
 
     void del_latinus();
 
@@ -41,3 +42,21 @@ namespace ivanov {
 int main() {
   return 0;
 }
+
+ivanov::Line::Line(): content(new char[1]), size(1)
+{
+  content[0] = 0;
+}
+ivanov::Line::~Line() {
+  delete[] content;
+}
+ivanov::Line &ivanov::Line::operator=(const Line l) {
+  char * tmp = new char[l.get_size()];
+  for (size_t i = 0; i < l.get_size(); ++i) {
+    tmp[i] = l.get(i);
+  }
+  delete[] content;
+  content = tmp;
+  size = l.get_size();
+}
+
