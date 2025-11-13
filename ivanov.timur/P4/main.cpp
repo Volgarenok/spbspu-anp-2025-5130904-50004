a5b7bd06a50689f08caa1b097bc4a8d1ad0aa821
isaacgreenfield <ivtim708@gmail.com>
2025-11-13 21:27:23 +0300

    Signed namespace ivanov

--- ivanov.timur/P4/main.cpp
+++ ivanov.timur/P4/main.cpp
@@ -17,6 +17,27 @@ size_t get_line(std::istream & in, char * data, size_t size) {
   return i;
 }
 
+namespace ivanov {
+  struct Line {
+    char * content;
+    size_t size;
+    Line();
+    ~Line();
+    Line & operator=(Line l);
+    void next(char s);
+    void space_cleaner();
+    void rmv(size_t id) noexcept;
+
+    size_t get_size() const noexcept;
+    void get_line() const noexcept;
+
+    void del_latinus();
+
+    char * get_letters();
+  };
+  char * unite_letters(char * letters1, char * letters2);
+}
+
 int main() {
   return 0;
 }
