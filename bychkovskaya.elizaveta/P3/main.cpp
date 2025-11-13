f5f13f7791c3b5d0cfb5875c422bba6cf6184c3a
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-13 21:35:39 +0300

    base1

--- bychkovskaya.elizaveta/P3/main.cpp
+++ bychkovskaya.elizaveta/P3/main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-#include <fstream>
+// #include <fstream>
 
-int main(int argc, char ** argv) 
+int main(int, char ** argv)
 {
-
-}
+	char num = argv[1][0];
+	if (num == '1') {
+		//массив фиксированной длины
+	}
+	if (num == '2') {
+		//динамический массив
+	}
+} 
