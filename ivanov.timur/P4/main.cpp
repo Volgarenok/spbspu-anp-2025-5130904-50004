ce9078c9036cadc9497120ea767286fe27d78378
isaacgreenfield <ivtim708@gmail.com>
2025-11-13 21:12:24 +0300

    Created get_line

--- ivanov.timur/P4/main.cpp
+++ ivanov.timur/P4/main.cpp
@@ -1,3 +1,22 @@
+#include<iostream>
+#include<iomanip>
+
+size_t get_line(std::istream & in, char * data, size_t size) {
+  bool is_skipws = in.flags() & std::ios_base::skipws;
+  if (is_skipws) {
+    in >> std::noskipws;
+  }
+  size_t i = 0;
+  for (; i < size; ++i) {
+    in >> data[i];
+  }
+  data[i] = 0;
+  if (is_skipws) {
+    in >> std::skipws;
+  }
+  return i;
+}
+
 int main() {
   return 0;
 }
