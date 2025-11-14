4db63affc38a43f560f01f5f5f5331d5eb51c206
polkan666 <cristalix56@mail.ru>
2025-11-15 00:47:54 +0300

    everything is new

--- merniy.ivan/P3/main.cpp
+++ merniy.ivan/P3/main.cpp
@@ -14,6 +14,21 @@ int main(int argc, char* argv[])
         matrix[i] = new int[cols];
         for (int j = 0; j < cols; j++) {
             input >> matrix[i][j];
+ }
+ int task = stoi(argv[1]);
+ int top = 0, bottom = rows - 1, left = 0, right = cols - 1;
+ int val = 1;
+ if (task == 5) {
+	 while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) matrix[top][i] -= val++;
+            top++;
+            for (int i = top; i <= bottom; i++) matrix[i][right] -= val++;
+            right--;
+            for (int i = right; i >= left && top <= bottom; i--) matrix[bottom][i] -= val++;
+            bottom--;
+            for (int i = bottom; i >= top && left <= right; i--) matrix[i][left] -= val++;
+            left++;
         }
-    }
+    } else if (task == 6) {
+
 
