0d56b9dd372c934157b50f6cc50becc9ab768681
ryouta25 <avp22.07@mail.ru>
2025-11-14 22:23:59 +0300

    Added function for LFT-BOT-CLK

--- pozdeev.andrey/P3/main.cpp
+++ pozdeev.andrey/P3/main.cpp
@@ -35,6 +35,58 @@ namespace pozdeev
         }
         return rowsWithoutConsecutive;
     }
+
+    void spiral(int* matrix, int rows, int cols)
+    {
+        if (rows <= 0 || cols <= 0)
+        {
+            return;
+        }
+
+        int top = 0, bottom = rows - 1;
+        int left = 0, right = cols - 1;
+        int decrement = 1; 
+
+        while (top <= bottom && left <= right)
+        {
+            for (int r = bottom; r >= top; --r)
+            {
+                matrix[r * cols + left] -= decrement;
+                decrement++;
+            }
+            if (++left > right)
+            {
+                break;
+            }
+
+            for (int c = left; c <= right; ++c)
+            {
+                matrix[top * cols + c] -= decrement;
+                decrement++;
+            }
+            if (++top > bottom)
+            {
+                break;
+            }
+
+            for (int r = top; r <= bottom; ++r)
+            {
+                matrix[r * cols + right] -= decrement;
+                decrement++;
+            }
+            if (--right < left)
+            {
+                break;
+            }
+
+            for (int c = right; c >= left; --c)
+            {
+                matrix[bottom * cols + c] -= decrement;
+                decrement++;
+            }
+            --bottom; 
+        }
+    }
 }
 
 int main()
