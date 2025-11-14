8b8f3a4ad2043b3f6aedd636e39fad86dd1e1fa3
emiraydgn <emiraydogan03@gmail.com>
2025-11-14 21:25:43 +0300

    Added NUM-COL-LSR function

--- aydogan.emir/P3/lab.cpp
+++ aydogan.emir/P3/lab.cpp
@@ -24,6 +24,38 @@ std::istream& readDynamic(std::istream& input, int* arr, int r, int c)
     return input;
 }
 
+int numColLsr(int r, int c, const int* a)
+{
+    if (r <= 0 || c <= 0) return 0;
+
+    int bestLen = 0;
+    int bestCol = 1;
+
+    for (int col = 0; col < c; ++col) {
+        int cur = 1;
+        int maxHere = 1;
+
+        for (int row = 1; row < r; ++row) {
+            int prev = a[(row - 1) * c + col];
+            int now  = a[row * c + col];
+
+            if (now == prev) {
+                ++cur;
+                if (cur > maxHere) maxHere = cur;
+            } else {
+                cur = 1;
+            }
+        }
+
+        if (maxHere > bestLen) {
+            bestLen = maxHere;
+            bestCol = col + 1;
+        }
+    }
+
+    return bestCol;
+}
+
 int main()
 {
     return 0;
