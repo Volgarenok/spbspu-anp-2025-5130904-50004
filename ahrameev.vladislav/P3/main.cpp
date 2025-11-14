85bd12a3996b76c46c581f8fe80fe397db861dd4
EIKO7 <camper1773@mail.ru>
2025-11-15 01:04:47 +0300

    ver 1: Check Arguments

--- ahrameev.vladislav/P3/main.cpp
+++ ahrameev.vladislav/P3/main.cpp
@@ -1,6 +1,28 @@
-#include <iostream> 
+#include <iostream>
+#include <fstream>
 
-int main()
-{
+namespace ahrameev {
+    bool checkArguments(int argc, char* argv[]) {
+        if (argc != 4) {
+            std::cerr << "Error: Wrong number of arguments\n";
+            return false;
+        }
 
+        std::string num = argv[1];
+        if (num != "1" && num != "2") {
+            std::cerr << "Error: First argument must be 1 or 2\n";
+            return false;
+        }
+
+        return true;
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (!ahrameev::checkArguments(argc, argv)) {
+        return 1;
+    }
+
+    std::cout << "Arguments are OK\n";
+    return 0;
 }
