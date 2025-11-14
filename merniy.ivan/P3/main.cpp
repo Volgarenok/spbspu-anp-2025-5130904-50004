6a792df8bc23a483f8f593e39f558691707b7821
polkan666 <cristalix56@mail.ru>
2025-11-15 00:59:39 +0300

    matrix2

--- merniy.ivan/P3/main.cpp
+++ merniy.ivan/P3/main.cpp
@@ -17,5 +17,17 @@ int main(int argc, char* argv[]) {
     int variant = stoi(argv[1]);
     int t = 0, b = n - 1, l = 0, r = m - 1;
     int k = 1;
+    if (variant == 5) {
+       while (t <= b && l <= r) {
+           for (int j = l; j <= r; j++) a[t][j] -= k++;
+           t++;
+           for (int i = t; i <= b; i++) a[i][r] -= k++;
+           r--;
+           for (int j = r; j >= l && t <= b; j--) a[b][j] -= k++;
+           b--;
+           for (int i = b; i >= t && l <= r; i--) a[i][l] -= k++;
+           l++;
+       }
+   } 
 
 
