c96f319346cf0d8b13b3b4fb5eceeb6aaec5f628
polkan666 <cristalix56@mail.ru>
2025-11-15 00:56:09 +0300

    matrix

--- merniy.ivan/P3/main.cpp
+++ merniy.ivan/P3/main.cpp
@@ -1,6 +1,21 @@
 #include <tream>
 #include <fsream>
 
-int main(int argc, char* argv[]) 
+int main(int argc, char* argv[]) {
+     if (argc != 4) return 1;
+    ifstream fin(argv[2]);
+    ofstream fout(argv[3]);
+    int n, m;
+    fin >> n >> m;
+    int** a = (int**)malloc(n * sizeof(int*));
+    for (int i = 0; i < n; i++) {
+        a[i] = (int*)malloc(m * sizeof(int));
+        for (int j = 0; j < m; j++) {
+            fin >> a[i][j];
+        }
+    }
+    int variant = stoi(argv[1]);
+    int t = 0, b = n - 1, l = 0, r = m - 1;
+    int k = 1;
 
 
