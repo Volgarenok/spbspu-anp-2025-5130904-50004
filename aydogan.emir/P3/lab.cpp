432edf572faf1a3262613a1b2f5c4acdf9a050ec
emiraydgn <emiraydogan03@gmail.com>
2025-11-14 20:54:48 +0300

    Added static matrix reading function

--- aydogan.emir/P3/lab.cpp
+++ aydogan.emir/P3/lab.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
 
-
 namespace aydogan {
 
+std::istream& readStatic(std::istream& input, int* arr, int r, int c)
+{
+    for (int i = 0; i < r * c; ++i) {
+        if (!(input >> arr[i])) {
+            std::cerr << "Incorrect matrix input\n";
+            return input;
+        }
+    }
+    return input;
 }
+
 
 int main() {
     return 0;
+}
+
 }
