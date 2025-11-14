4b1b167c5227ee1e163d9cb64b4b46767d2db9d5
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-14 20:27:07 +0300

    fix CG5

--- bychkovskaya.elizaveta/P3/main.cpp
+++ bychkovskaya.elizaveta/P3/main.cpp
@@ -32,7 +32,7 @@ int* create(size_t rows, size_t cols)
   return result;
 }
 
-std::ifstream & inputMatrix(std::ifstream & in, int * m, size_t rows, size_t cols) 
+std::ifstream & inputMatrix(std::ifstream & in, int * m, size_t rows, size_t cols)
 {
   for (size_t i = 0; i < rows; ++i) {
     for (size_t j = 0; j < cols; ++j) {
