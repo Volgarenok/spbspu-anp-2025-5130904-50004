76d26b4a416e12af95a551b0ef7010856ac8095e
EIKO7 <camper1773@mail.ru>
2025-11-15 02:00:40 +0300

    ver 4 Write result

--- ahrameev.vladislav/P3/main.cpp
+++ ahrameev.vladislav/P3/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 #include <fstream>
 
-namespace ahrameev {
-    bool checkArguments(int argc, char* argv[]) {
-        if (argc != 4) {
+namespace ahrameev
+{
+    bool checkArguments(int argc, char* argv[])
+    {
+        if (argc != 4)
+        {
             std::cerr << "Error: Wrong number of arguments\n";
             return false;
         }
 
         std::string num = argv[1];
-        if (num != "1" && num != "2") {
+        if (num != "1" && num != "2")
+        {
             std::cerr << "Error: First argument must be 1 or 2\n";
             return false;
         }
@@ -17,15 +21,18 @@ namespace ahrameev {
         return true;
     }
 
-    bool readMatrixSize(std::ifstream& input, int& rows, int& cols, const std::string& num) {
+    bool readMatrixSize(std::ifstream& input, int& rows, int& cols, const std::string& num)
+    {
         input >> rows >> cols;
 
-        if (rows < 0 || cols < 0) {
+        if (rows < 0 || cols < 0)
+        {
             std::cerr << "Error: Wrong matrix size\n";
             return false;
         }
 
-        if (num == "1" && rows * cols > 10000) {
+        if (num == "1" && rows * cols > 10000)
+        {
             std::cerr << "Error: Matrix too big for fixed array\n";
             return false;
         }
@@ -33,17 +40,23 @@ namespace ahrameev {
         return true;
     }
 
-    int** createMatrix(int rows, int cols, const std::string& num) {
+    int** createMatrix(int rows, int cols, const std::string& num)
+    {
         int** matrix;
 
-        if (num == "1") {
-            matrix = new int*[100];
-            for (int i = 0; i < 100; i++) {
+        if (num == "1")
+        {
+            matrix = new int* [100];
+            for (int i = 0; i < 100; i++)
+            {
                 matrix[i] = new int[100];
             }
-        } else {
-            matrix = new int*[rows];
-            for (int i = 0; i < rows; i++) {
+        }
+        else
+        {
+            matrix = new int* [rows];
+            for (int i = 0; i < rows; i++)
+            {
                 matrix[i] = new int[cols];
             }
         }
@@ -51,32 +64,57 @@ namespace ahrameev {
         return matrix;
     }
 
-    void freeMatrix(int** matrix, const std::string& num, int rows) {
-        if (num == "1") {
-            for (int i = 0; i < 100; i++) {
+    void freeMatrix(int** matrix, const std::string& num, int rows)
+    {
+        if (num == "1")
+        {
+            for (int i = 0; i < 100; i++)
+            {
                 delete[] matrix[i];
             }
-        } else {
-            for (int i = 0; i < rows; i++) {
+        }
+        else
+        {
+            for (int i = 0; i < rows; i++)
+            {
                 delete[] matrix[i];
             }
         }
         delete[] matrix;
     }
 
-    int calculateSum(int** matrix, int rows, int cols) {
+    int calculateSum(int** matrix, int rows, int cols)
+    {
         int sum = 0;
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
                 sum += matrix[i][j];
             }
         }
         return sum;
     }
+
+    void writeResult(std::ofstream& output, int** matrix, int rows, int cols, int sum)
+    {
+        output << "Sum: " << sum << "\n";
+        output << "Matrix:\n";
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                output << matrix[i][j] << " ";
+            }
+            output << "\n";
+        }
+    }
 }
 
-int main(int argc, char* argv[]) {
-    if (!ahrameev::checkArguments(argc, argv)) {
+int main(int argc, char* argv[])
+{
+    if (!ahrameev::checkArguments(argc, argv))
+    {
         return 1;
     }
 
@@ -85,23 +123,26 @@ int main(int argc, char* argv[]) {
     std::string outputFile = argv[3];
 
     std::ifstream input(inputFile);
-    if (!input.is_open()) {
+    if (!input.is_open())
+    {
         std::cerr << "Error: Cannot open input file\n";
         return 2;
     }
 
     int rows, cols;
-    if (!ahrameev::readMatrixSize(input, rows, cols, num)) {
+    if (!ahrameev::readMatrixSize(input, rows, cols, num))
+    {
         return 2;
     }
 
-
     int** matrix = ahrameev::createMatrix(rows, cols, num);
 
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            if (!(input >> matrix[i][j])) {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (!(input >> matrix[i][j]))
+            {
                 std::cerr << "Error: Wrong matrix data\n";
                 ahrameev::freeMatrix(matrix, num, rows);
                 return 2;
@@ -110,12 +151,21 @@ int main(int argc, char* argv[]) {
     }
     input.close();
 
-
     int sum = ahrameev::calculateSum(matrix, rows, cols);
-    std::cout << "Sum of elements: " << sum << "\n";
 
+    std::ofstream output(outputFile);
+    if (!output.is_open())
+    {
+        std::cerr << "Error: Cannot open output file\n";
+        ahrameev::freeMatrix(matrix, num, rows);
+        return 2;
+    }
+
+    ahrameev::writeResult(output, matrix, rows, cols, sum);
+    output.close();
 
     ahrameev::freeMatrix(matrix, num, rows);
+
+    std::cout << "Program finished successfully!\n";
     return 0;
 }
-
