aea525006f80417bf9f5a30868a694853ca46d5b
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-14 19:41:59 +0300

    fix CG

--- bychkovskaya.elizaveta/P3/main.cpp
+++ bychkovskaya.elizaveta/P3/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 
-bool ifNumber (char ** m) {
-	size_t k = 0;
+bool ifNumber(char ** m) 
+{ 
+  size_t k = 0;
 	size_t k1 = 0;
 	for (size_t i = 0; m[1][i]!='\0'; ++i) {
 		for (size_t j = 0; j < 10; j++) {
@@ -15,24 +16,24 @@ bool ifNumber (char ** m) {
 		++k1;
 	}
 	if (k==k1) {
-			return true;
+		return true;
 	}
 	return false;
 }
 
-int * create (size_t rows, size_t cols)
+int* create(size_t rows, size_t cols)
 {
   int * result = nullptr;
   try {
     result = new int [rows*cols];
-  }
-	catch (...) {
+  } catch (...) {
 		throw "Not enough memory";
 	}
 	return result;
 }
 
-std::ifstream& inputMatrix (std::ifstream& in, int * m, size_t rows, size_t cols) {
+std::ifstream & inputMatrix(std::ifstream & in, int * m, size_t rows, size_t cols) 
+{
 	for (size_t i = 0; i < rows; ++i) {
 		for (size_t j = 0; j < cols; ++j) {
 			in >> m[i*cols+j];
@@ -44,7 +45,8 @@ std::ifstream& inputMatrix (std::ifstream& in, int * m, size_t rows, size_t cols
 	return in;
 }
 
-size_t CNT_COL_NSM (int * m, size_t rows, size_t cols) {
+size_t CNT_COL_NSM(int * m, size_t rows, size_t cols) 
+{
   size_t count = 0;
   for (size_t j = 0; j < cols; ++j) {
     size_t countcol = 0;
@@ -53,26 +55,26 @@ size_t CNT_COL_NSM (int * m, size_t rows, size_t cols) {
         ++countcol;
       }
     }
-    if (countcol==0) {
+    if (countcol == 0) {
       ++count;
     }
   }
   return count;
  }
 
-void cut_to_square_DYNAMIC(int*& m, size_t& rows, size_t& cols) {
+void cut_to_square_DYNAMIC(int*& m, size_t& rows, size_t& cols) 
+{
     if (rows > cols) {
         int* tmp = create(cols, cols);
         for (size_t i = 0; i < cols; ++i) {
             for (size_t j = 0; j < cols; ++j) {
-                tmp[i * cols + j] = m[i * cols + j];
+              tmp[i * cols + j] = m[i * cols + j];
             }
         }
         delete[] m;
         m = tmp;
         rows = cols;
-    }
-    else if (rows < cols) {
+    } else if (rows < cols) {
         int* tmp = create(rows, rows);
         for (size_t i = 0; i < rows; ++i) {
             for (size_t j = 0; j < rows; ++j) {
@@ -85,16 +87,17 @@ void cut_to_square_DYNAMIC(int*& m, size_t& rows, size_t& cols) {
     }
 }
 
-void cut_to_square_FIXED(size_t& rows, size_t& cols) {
+void cut_to_square_FIXED(size_t& rows, size_t& cols) 
+{
     if (rows > cols) {
         rows = cols;
-    }
-    if (rows < cols) {
+    } else if (rows < cols) {
         cols = rows;
     }
 }
 
-size_t CNT_NZR_DIG_DYNAMIC(int*& m, size_t& rows, size_t& cols) {
+size_t CNT_NZR_DIG_DYNAMIC(int*& m, size_t& rows, size_t& cols) 
+{
     cut_to_square_DYNAMIC(m, rows, cols);
     size_t count = 0;
     int k = -int(rows) + 1;
@@ -136,7 +139,7 @@ size_t CNT_NZR_DIG_FIXED(int* m, size_t& rows, size_t& cols) {
                 }
             }
         }
-        if (countdiag == 0 && k!=0) {
+        if (countdiag == 0 && k != 0) {
             ++count;
         }
         ++k;
@@ -149,8 +152,7 @@ int main(int argc, char ** argv)
 	if (argc > 4) {
 		std::cerr << "Too many arguments" << "\n";
 		return 1;
-	}
-	else if (argc < 4) {
+  } else if (argc < 4) {
 		std::cerr << "Not enough arguments" << "\n";
     return 1;
 	}
@@ -168,8 +170,7 @@ int main(int argc, char ** argv)
 		  int fixedmatrix[10000];
       try {
         inputMatrix (input, fixedmatrix, rows, cols);
-      }
-      catch (const char* e) {
+      } catch (const char* e) {
         std::cerr << e << "\n";
         return 2;
       }
@@ -178,13 +179,11 @@ int main(int argc, char ** argv)
       output << CNT_COL_NSM (fixedmatrix, rows, cols) << "\n";
       output << CNT_NZR_DIG_FIXED(fixedmatrix, rows, cols) << "\n";
       output.close();
-		}
-		else if (num == '2') {
+		} else if (num == '2') {
       int * dynamicmatrix = nullptr;
       try {
 			  dynamicmatrix = create (rows, cols);
-      }
-      catch (const char* e) {
+      } catch (const char* e) {
         std::cerr << e << "\n";
         return 2;
       }
@@ -195,22 +194,20 @@ int main(int argc, char ** argv)
         output << CNT_COL_NSM (dynamicmatrix, rows, cols) << "\n"; 
         output << CNT_NZR_DIG_DYNAMIC(dynamicmatrix, rows, cols) << "\n";
 			  delete[] dynamicmatrix;
-      }
-      catch (const char* e) {
+      } catch (const char* e) {
         std::cerr << e << "\n";
         delete[] dynamicmatrix;
         return 2;
       }
-    }
-	}
-	else {
-		if (ifNumber (argv) == 1) {
+    } else {
+      if (ifNumber (argv) == 1) {
 				std::cerr << "First parameter is out of range" << "\n";
 				return 1;
-		}
-		if (ifNumber (argv) == 0) {
+      }
+		  if (ifNumber (argv) == 0) {
 				std::cerr << "First parameter is not a number" << "\n";
 				return 1;
-		}
-	}
+      }
+	  }
+  }
 }
