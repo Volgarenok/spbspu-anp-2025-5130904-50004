72ea5c74366ceb384d27dd65c89c2be873cdb6b3
polkan666 <cristalix56@mail.ru>
2025-11-15 00:41:48 +0300

    clockwise, decreasing

--- merniy.ivan/P3/main.cpp
+++ merniy.ivan/P3/main.cpp
@@ -1,3 +1,19 @@
-int main()
+#include <iostream>
+#include <fsream>
+
+int main(int argc, char* argv[]) 
 {
-}
+ if (argc < 4) return 1;
+ if (string(argv[1]) != "2") return 1;
+ ifstream input(argv[2]);
+ ofstream output(argv[3]);
+ int rows, cols;
+ input >> rows >> cols;
+ int** matrix = new int*[rows];
+    for (int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+        for (int j = 0; j < cols; j++) {
+            input >> matrix[i][j];
+        }
+    }
+
