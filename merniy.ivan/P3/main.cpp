cc03181722e495287ee71c854d98fd29e81a0ad2
polkan666 <cristalix56@mail.ru>
2025-11-15 01:27:35 +0300

    P3 complete

--- merniy.ivan/P3/main.cpp
+++ merniy.ivan/P3/main.cpp
@@ -1,35 +1,35 @@
 #include <iostream>
 #include <fstream>
+#include <cstdlib>
 
 int main(int argc, char* argv[]) {
-     if (argc != 4) return 1;
-    ifstream fin(argv[2]);
-    ofstream fout(argv[3]);
+    if (argc != 4) return 1;
+    std::ifstream fin(argv[2]);
+    std::ofstream fout(argv[3]);
     int n, m;
     fin >> n >> m;
-    int** a = (int**)malloc(n * sizeof(int*));
+    int** a = static_cast<int**>(std::malloc(n * sizeof(int*)));
     for (int i = 0; i < n; i++) {
-        a[i] = (int*)malloc(m * sizeof(int));
+        a[i] = static_cast<int*>(std::malloc(m * sizeof(int)));
         for (int j = 0; j < m; j++) {
             fin >> a[i][j];
         }
     }
-    int variant = stoi(argv[1]);
+    int variant = std::stoi(argv[1]);
     int t = 0, b = n - 1, l = 0, r = m - 1;
     int k = 1;
     if (variant == 5) {
-       while (t <= b && l <= r) {
-           for (int j = l; j <= r; j++) a[t][j] -= k++;
-           t++;
-           for (int i = t; i <= b; i++) a[i][r] -= k++;
-           r--;
-           for (int j = r; j >= l && t <= b; j--) a[b][j] -= k++;
-           b--;
-           for (int i = b; i >= t && l <= r; i--) a[i][l] -= k++;
-           l++;
-       }
-   }
-   else if (variant == 6) {
+        while (t <= b && l <= r) {
+            for (int j = l; j <= r; j++) a[t][j] -= k++;
+            t++;
+            for (int i = t; i <= b; i++) a[i][r] -= k++;
+            r--;
+            for (int j = r; j >= l && t <= b; j--) a[b][j] -= k++;
+            b--;
+            for (int i = b; i >= t && l <= r; i--) a[i][l] -= k++;
+            l++;
+        }
+    } else if (variant == 6) {
         while (t <= b && l <= r) {
             for (int i = t; i <= b; i++) a[i][l] += k++;
             l++;
@@ -47,7 +47,7 @@ int main(int argc, char* argv[]) {
             fout << " " << a[i][j];
         }
     }
-    for (int i = 0; i < n; i++) free(a[i]);
-    free(a);
+    for (int i = 0; i < n; i++) std::free(a[i]);
+    std::free(a);
     return 0;
 }
