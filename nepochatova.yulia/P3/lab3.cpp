0048cf3dcde181100448487f01f9259a07f2d63e
GenJune <julia.nepochatova@gmail.com>
2025-11-14 19:53:29 +0300

    create readMatrix

--- nepochatova.yulia/P3/lab3.cpp
+++ nepochatova.yulia/P3/lab3.cpp
@@ -1,1 +1,15 @@
-int main()
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+
+namespace nepochatova {
+    std::istream& readMatrix(std::istream& input, int** m, size_t rows, size_t cols) {
+        for (size_t i = 0; i < rows; ++i) {
+            for (size_t j = 0; j < cols; ++j) {
+                if (!(input >> m[i][j])) {
+                    throw std::runtime_error("Incorrect matrix input");
+                }
+            }
+        }
+        return input;
+    }
