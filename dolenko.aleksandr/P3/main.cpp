bc19dfe1f591c799f50d39b83cbbd36389768751
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-15 17:43:51 +0300

    check argc

--- dolenko.aleksandr/P3/main.cpp
+++ dolenko.aleksandr/P3/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 
-int main()
-{
+int main(int argc, char ** argv) {
+    if (argc > 4) {
+        std::cerr <<"Too many arguments\n";
+        return 1;
+    }
+    else if (argc < 4) {
+        std::cerr <<"Not enough arguments\n";
+        return 1;
+    }
 
+    
+    return 0;
 }
