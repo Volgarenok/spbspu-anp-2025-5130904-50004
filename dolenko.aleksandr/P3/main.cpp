d043e2d6a8d67378ba584bc04f2bac3dab8397fc
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2025-11-15 19:03:05 +0300

    argument checking function

--- dolenko.aleksandr/P3/main.cpp
+++ dolenko.aleksandr/P3/main.cpp
@@ -2,16 +2,44 @@
 #include <fstream>
 #include <cstdlib>
 
-int main(int argc, char ** argv) {
-    if (argc > 4) {
-        std::cerr <<"Too many arguments\n";
-        return 1;
+bool parse_num_arg(const char * str, int & num) {
+    if (str[1] != "0") {
+        return false;
     }
-    else if (argc < 4) {
-        std::cerr <<"Not enough arguments\n";
+    if (str[0] == '1') {
+        num = 1;
+        return true;
+    }
+    if (str[0] == '2') {
+        num = 2;
+        return true;
+    }
+    return false;
+}
+
+bool validate_arguments(int argc, char ** argv, int & num, const char *& input, const char *& output) {
+    if (argc != 4) {
+        std::cerr << "Not enough arguments or too many arguments"<< std::endl;
+        return false;
+    }
+    if (!parse_num_arg(argv[1], num)) {
+        std::cerr << "First parameter is not a number or first parameter is out of range" << std::endl;
+        return false;
+    }
+    input = argv[2];
+    output = argv[3];
+
+    return true;
+}
+
+int main(int argc, char ** argv) {
+    int num = 0;
+    const char * input = nullptr;
+    const char * output = nullptr;
+
+    if (!validate_arguments(argc, argv, num, input, output)) {
         return 1;
     }
 
-    
-    return 0;
-}
+        return 0;
+    }
