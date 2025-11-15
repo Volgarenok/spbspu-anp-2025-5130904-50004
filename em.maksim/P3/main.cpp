ff66a2919b95bee0d1d0a67d48bedee3b8960601
Emich-M <fagetolrer@gmail.com>
2025-11-15 17:26:55 +0300

    Basic matrix writing to file

--- em.maksim/P3/main.cpp
+++ em.maksim/P3/main.cpp
@@ -83,6 +83,26 @@ bool readMatrix(const char* filename, int*** matrix, int& rows, int& cols) {
     return true;
 }
 
+bool writeMatrix(const char* filename, int** matrix, int rows, int cols) {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cerr << "Cannot open output file: " << filename << std::endl;
+        return false;
+    }
+    
+    file << rows << " " << cols;
+    
+    if (rows > 0 && cols > 0 && matrix) {
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                file << " " << matrix[i][j];
+            }
+        }
+    }
+    
+    return true;
+}
+
 } // namespace em
 
 int main(int argc, char* argv[]) {
