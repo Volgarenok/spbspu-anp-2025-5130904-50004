2e6bf132e384bbdcec8cc0abe8496ccea0f19a68
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-16 22:41:37 +0300

    teper eto ne massiv iz ukazateley, a normalniu massiv i odnomernii

--- kuchukbaeva.alyona/P3/main.cpp
+++ kuchukbaeva.alyona/P3/main.cpp
@@ -30,59 +30,70 @@ int main(int argc, char* argv[])
     std::cerr << "First parameter is out of range" << "\n";
     return 1;
   }
-  const char* inputFile = argv[2];
-  const char* outputFile = argv[3];
+    std::ifstream in(argv[2]);
+  if (!in.is_open())
+  {
+    std::cerr << "Cannot open input file" << "\n";
+    return 2;
+  }
+
+  std::ofstream out(argv[3]);
+  if (!out.is_open())
+  {
+    std::cerr << "Cannot open output file" << "\n";
+    return 2;
+  }
+
   size_t rows = 0;
   size_t cols = 0;
 
+  if (!(in >> rows >> cols))
+  {
+    std::cerr << "Cannot read matrix dimensions" << "\n";
+    return 2;
+  }
+
+  if (rows == 0 || cols == 0)
+  {
+    kuchukbaeva::writeMatrix(out, nullptr, 0, 0, 0);
+    return 0;
+  }
+
+  const size_t matrixSize = rows * cols;
+  int* matrix = nullptr;
+  int static_matrix[10000] = {};
   if (num == 1)
   {
-    const size_t stm = 100;
-    int static_matrix[stm][stm];
-    int* row_po[stm];
-    for (size_t i = 0; i < stm; ++i)
-    {
-      row_po[i] = static_matrix[i];
-    }
-    if (!kuchukbaeva::readMatrixStatic(inputFile, row_po, rows, cols, stm, stm))
-    {
-      std::cerr << "Cannot read matrix from file" << "\n";
-      return 2;
-    }
-    if (rows == 0 || cols == 0)
-    {
-      kuchukbaeva::Res(outputFile, 0);
-    }
-    else
-    {
-      int locMaxCount = kuchukbaeva::countLocMax(row_po, rows, cols);
-      kuchukbaeva::LftBotClk(row_po, rows, cols);
-      kuchukbaeva::writeMatrix(outputFile, row_po, rows, cols, locMaxCount);
-    }
+    matrix = static_matrix;
   }
   else
   {
-    int** matrix = nullptr;
-    if (!kuchukbaeva::readMatrix(inputFile, &matrix, rows, cols))
-    {
-      std::cerr << "Cannot read matrix from file" << "\n";
-      return 2;
-    }
+    matrix = static_cast<int*>(malloc(matrixSize * sizeof(int)));
     if (matrix == nullptr)
     {
-      kuchukbaeva::Res(outputFile, 0);
-    }
-    else
-    {
-      int locMaxCount = kuchukbaeva::countLocMax(matrix, rows, cols);
-      kuchukbaeva::LftBotClk(matrix, rows, cols);
-      kuchukbaeva::writeMatrix(outputFile, matrix, rows, cols, locMaxCount);
-    }
-    if (matrix != nullptr)
-    {
-      kuchukbaeva::freMatrix(matrix, rows);
+      std::cerr << "Troubles with memory" << "\n";
+      return 2;
     }
   }
+
+  if (!kuchukbaeva::readMatrix(in, matrix, rows, cols))
+  {
+    std::cerr << "Cannot reading matrix" << "\n";
+    in.close();
+    if (num == 2)
+    {
+      free(matrix);
+    }
+    return 2;
+  }
+  in.close();
+  int locMaxCount = kuchukbaeva::countLocMax(matrix, rows, cols);
+  kuchukbaeva::LftBotClk(matrix, rows, cols);
+  kuchukbaeva::writeMatrix(out, matrix, rows, cols, locMaxCount);
+  if (num == 2)
+  {
+    free(matrix);
+  }
+
   return 0;
 }
-
