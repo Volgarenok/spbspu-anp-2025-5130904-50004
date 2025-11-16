4ac70a61d8a4f8e9e77f39a07b7735b141d8b7ac
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-16 22:45:44 +0300

    ispravila header guard

--- kuchukbaeva.alyona/P3/functions.hpp
+++ kuchukbaeva.alyona/P3/functions.hpp
@@ -1,16 +1,17 @@
-#ifndef FUNCTIONS2_HPP
-#define FUNCTIONS2_HPP
-#include <cstdlib>
+#ifndef FUNCTIONS_HPP
+#define FUNCTIONS_HPP
+#include <cstddef>
+#include <iosfwd>
+
 namespace kuchukbaeva
 {
-  bool CntLocMax(int** matrix, size_t rows, size_t cols, size_t i, size_t j);
-  int countLocMax(int** matrix, size_t rows, size_t cols);
-  void LftBotClk(int** matrix, size_t rows, size_t cols);
-  bool readMatrix(const char* filename, int*** matrix, size_t& rows, size_t& cols);
-  bool readMatrixStatic(const char* filename, int** matrix, size_t& rows, size_t& cols, size_t max_rows, size_t max_cols);
-  void freMatrix(int** matrix, size_t rows);
-  bool Res(const char* filename, int res);
-  bool writeMatrix(const char* filename, int** matrix, size_t rows, size_t cols, int locMaxCount);
+  bool CntLocMax(const int* matrix, size_t rows, size_t cols, size_t i, size_t j);
+  int countLocMax(const int* matrix, size_t rows, size_t cols);
+  void LftBotClk(int* matrix, size_t rows, size_t cols);
+  std::istream& readMatrix(std::istream& in, int* matrix, size_t rows, size_t cols);
+  std::ostream& writeMatrix(std::ostream& out, const int* matrix, size_t rows, size_t cols, int locMaxCount);
 }
+
 #endif
+
 
