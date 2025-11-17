7146e2387d6a98e478b3d7573645f247bde41f8d
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-17 22:11:39 +0300

    I deleted mistakes and divided the program into translation units

---
+++ khairullin.vadim/P3/functions.cpp
@@ -0,0 +1,108 @@
+#include "functions.hpp"
+#include <iostream>
+#include <fstream>
+
+namespace khairullin
+{
+  void filling(int * array, std::ifstream & input, int rows, int cols, int command)
+  {
+    for (int i = 0; i < rows * cols; ++i)
+    {
+      input >> array[i];
+      if (input.fail() or input.eof())
+      {
+        if (command == 2)
+        {
+          free(array);
+        }
+        throw std::invalid_argument("Error: file includes the mistake");
+      }
+    }
+    input.close();
+  }
+  int CheckZero(const int * array, int index1, int index2, int size)
+  {
+    if (array[index1 * size + index2] == array[index1 * size + index2 + 1] && array[index1 * size + index2] == 0)
+    {
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+  int localmax(const int * array, int rows, int cols)
+  {
+    int counter_of_max = 0;
+    for (int i = 0; i < rows; ++i)
+    {
+      for (int j = 0; j < cols; ++j)
+      {
+        if (i != 0 && j != 0 && i != rows - 1 && j != cols - 1)
+        {
+          if (array[i * cols + j] > array[(i - 1) * cols + j]
+              && array[i * cols + j] > array[(i + 1) * cols
+                  + j] && array[i * cols + j] > array[i *
+                      cols + j - 1] && array[i * cols + j] >
+                          array[i * cols + j + 1])
+          {
+            counter_of_max++;
+          }
+        }
+      }
+    }
+    return counter_of_max;
+  }
+  bool triangle(const int * array, int size)
+  {
+    int sum_of_zeroes = 0;
+    int count_of_nulls_on_row = size - 1;
+    int counter = 0;
+    const int COUNT_OF_ROWS_WITH_ZEROES = size - 1;
+    for (int i = 0; i < COUNT_OF_ROWS_WITH_ZEROES; ++i)
+    {
+      for (int j = 0; j < size; ++j)
+      {
+        if (j != size - 1)
+        {
+          counter += khairullin::CheckZero(array, i, j, size);
+        }
+      }
+      if (count_of_nulls_on_row - 1 == counter)
+      {
+        sum_of_zeroes++;
+        counter = 0;
+        count_of_nulls_on_row--;
+      }
+    }
+    return (sum_of_zeroes == COUNT_OF_ROWS_WITH_ZEROES);
+  }
+  int check_arguments(int argc, char ** argv, int command)
+  {
+    if (argc < 4)
+    {
+      std::cerr << "Not enough arguments\n";
+      return 1;
+    }
+    else if (argc > 4)
+    {
+      std::cerr << "Too many arguments\n";
+      return 1;
+    }
+    else if (argv[1][1] != '\0')
+    {
+      std::cerr << "First parameter is not a number\n";
+      return 1;
+    }
+    else if (command > 2 or command < 1)
+    {
+      std::cerr << "First parameter is out of range\n";
+      return 1;
+    }
+    else if (command == 1 or command == 2)
+    {
+      return 0;
+    }
+    return 0;
+  }
+}
---
+++ khairullin.vadim/P3/main.cpp
@@ -0,0 +1,120 @@
+#include "functions.hpp"
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+
+namespace khair = khairullin;
+int main (int argc, char ** argv)
+{
+  int command = 0;
+  if (argc != 1)
+  {
+    command = argv[1][0] - '0';
+  }
+  else
+  {
+    std::cerr << "No arguments\n";
+    return 1;
+  }
+  try
+  {
+    if (khair::check_arguments(argc, argv, command))
+    {
+      return 1;
+    }
+    std::ifstream input(argv[2]);
+    std::ofstream output(argv[3]);
+    int rows = 0, cols = 0;
+    input >> rows >> cols;
+    const int MIN_SIZE = std::min(rows, cols);
+    const int MAX_SIZE = std::max(rows, cols);
+    if (!input)
+    {
+      std::cerr << "File is empty\n";
+      return 2;
+    }
+    else if ((rows == 0 and cols != 0) || (rows != 0 and cols == 0) || (rows < 0 or cols < 0))
+    {
+      std::cerr << "Invalid rows or columns\n";
+      return 2;
+    }
+    else if (rows != 0 and cols != 0)
+    {
+      if (argv[1][0] == '1')
+      {
+        int array[10000] = {};
+        khair::filling(array, input, rows, cols, command);
+        output << khairullin::localmax(array, rows, cols) << "\n";
+        int square_array[10000] = {};
+        for (int i = 0; i < MIN_SIZE; ++i)
+        {
+          for (int j = 0; j < MIN_SIZE; ++j)
+          {
+              square_array[i * MIN_SIZE + j] = array[i * MAX_SIZE + j];
+          }
+        }
+        if (khair::triangle(square_array, MIN_SIZE))
+        {
+          output << "True\n";
+        }
+        else
+        {
+          output << "False\n";
+        }
+      }
+      else if (argv[1][0] == '2')
+      {
+        int * array = reinterpret_cast<int *> (std::malloc(rows * cols * sizeof(int)));
+        if (array == nullptr)
+        {
+          throw std::bad_alloc();
+        }
+        khair::filling(array, input, rows, cols, command);
+        output << khairullin::localmax(array, rows, cols) << "\n";
+        int * square_array = reinterpret_cast<int *>(std::malloc(MIN_SIZE * MIN_SIZE * sizeof(int)));
+        if (square_array == nullptr)
+        {
+          free(array);
+          throw std::bad_alloc();
+        }
+        for (int i = 0; i < MIN_SIZE; i++)
+        {
+          for (int j = 0; j < MIN_SIZE; ++j)
+          {
+            square_array[MIN_SIZE * i + j] = array[MAX_SIZE * i + j];
+          }
+        }
+        free(array);
+        if (khairullin::triangle(square_array, MIN_SIZE))
+        {
+          output << "True\n";
+        }
+        else
+        {
+          output << "False\n";
+        }
+        free(square_array);
+      }
+    }
+    else if (rows == 0 and cols == 0)
+    {
+      output << "Count of local maximums - 0\n";
+      output << "False\n";
+    }
+  }
+  catch(const std::invalid_argument & msg)
+  {
+    std::cerr << "ERROR: " << msg.what() << "\n";
+    return 2;
+  }
+  catch(const std::bad_alloc & e)
+  {
+    std::cerr << "ERROR: " << e.what() << "\n";
+    return 2;
+  }
+  catch(...)
+  {
+    std::cerr << "Error\n";
+    return 2;
+  }
+}
