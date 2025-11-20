f4a11e8d7945290ef02b9d2aa308649ab7569986
ryouta25 <avp22.07@mail.ru>
2025-11-20 21:15:59 +0300

    Убрал лишние пробелы2

--- pozdeev.andrey/P3/pozdeev.hpp
+++ pozdeev.andrey/P3/pozdeev.hpp
@@ -8,7 +8,6 @@ namespace pozdeev {
   constexpr int MAX_SIZE = 10000;
   constexpr int ERROR_INVALID_FORMAT = 91;
   constexpr int ERROR_INVALID_VALUE = 92;
-
   int countNonSequentialRows(const int* matrix, size_t rows, size_t cols);
   void spiral(int* matrix, int rows, int cols);
   int numAnalysis(const char* s);
