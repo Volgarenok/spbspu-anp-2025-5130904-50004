fff6f7e35feac6166f41dcd065d3b3badf9d8226
ryouta25 <avp22.07@mail.ru>
2025-11-20 20:50:23 +0300

    Updated pozdeev.hpp

--- pozdeev.andrey/P3/pozdeev.hpp
+++ pozdeev.andrey/P3/pozdeev.hpp
@@ -4,15 +4,17 @@
 #include <iostream>
 #include <fstream>
 
-namespace pozdeev
-{
-constexpr int MAX_SIZE = 10000;
+namespace pozdeev {
+  constexpr int MAX_SIZE = 10000;
+  
+  constexpr int ERROR_INVALID_FORMAT = 91;
+  constexpr int ERROR_INVALID_VALUE = 92;
 
-int countNonSequentialRows(const int* matrix, int rows, int cols);
-void spiral(int* matrix, int rows, int cols);
-int numAnalysis(const char* s, int& outVal);
-int processStatic(std::ifstream& fin, std::ofstream& fout, int rows, int cols);
-int processDynamic(std::ifstream& fin, std::ofstream& fout, int rows, int cols);
+  int countNonSequentialRows(const int* matrix, size_t rows, size_t cols);
+  void spiral(int* matrix, int rows, int cols);
+  int numAnalysis(const char* s);
+  int processStatic(std::ifstream& fin, std::ofstream& fout, size_t rows, size_t cols);
+  int processDynamic(std::ifstream& fin, std::ofstream& fout, size_t rows, size_t cols);
 }
 
 #endif
