2210c174ed2d22592231bfe207f21a9011a437eb
ryouta25 <avp22.07@mail.ru>
2025-11-20 20:52:42 +0300

    Updated pozdeev.cpp

--- pozdeev.andrey/P3/pozdeev.cpp
+++ pozdeev.andrey/P3/pozdeev.cpp
@@ -1,197 +1,163 @@
 #include "pozdeev.hpp"
+#include <iostream>
+#include <cctype>
 
 namespace pozdeev
 {
-int countNonSequentialRows(const int* matrix, int rows, int cols)
+int countNonSequentialRows(const int* matrix, size_t rows, size_t cols)
 {
-    if (rows <= 0 || cols <= 0)
-    {
-        return 0;
-    }
+  if (rows == 0 || cols == 0) {
+    return 0;
+  }
 
-    int rowsWithoutConsecutive = 0;
+  int rowsWithoutConsecutive = 0;
 
-    for (size_t r = 0; r < rows; ++r)
-    {
-        bool hasConsecutive = false;
-        for (size_t c = 0; c < cols - 1; ++c)
-        {
-            if (matrix[r * cols + c] == matrix[r * cols + c + 1])
-            {
-                hasConsecutive = true;
-                break;
-            }
-        }
-        if (!hasConsecutive)
-        {
-            rowsWithoutConsecutive++;
-        }
+  for (size_t r = 0; r < rows; ++r) {
+    bool hasConsecutive = false;
+    for (size_t c = 0; c < cols - 1; ++c) {
+      if (matrix[r * cols + c] == matrix[r * cols + c + 1]) {
+        hasConsecutive = true;
+        break;
+      }
     }
-    return rowsWithoutConsecutive;
+    if (!hasConsecutive) {
+      rowsWithoutConsecutive++;
+    }
+  }
+  return rowsWithoutConsecutive;
 }
 
 void spiral(int* matrix, int rows, int cols)
 {
-    if (rows <= 0 || cols <= 0)
-    {
-        return;
+  if (rows <= 0 || cols <= 0) {
+    return;
+  }
+
+  int top = 0, bottom = rows - 1;
+  int left = 0, right = cols - 1;
+  int decrement = 1;
+
+  while (top <= bottom && left <= right) {
+    for (int r = bottom; r >= top; --r) {
+      matrix[r * cols + left] -= decrement;
+      decrement++;
+    }
+    if (++left > right) {
+      break;
     }
 
-    int top = 0, bottom = rows - 1;
-    int left = 0, right = cols - 1;
-    int decrement = 1;
-
-    while (top <= bottom && left <= right)
-    {
-        for (size_t r = bottom; r >= top; --r)
-        {
-            matrix[r * cols + left] -= decrement;
-            decrement++;
-        }
-        if (++left > right)
-        {
-            break;
-        }
-
-        for (size_t c = left; c <= right; ++c)
-        {
-            matrix[top * cols + c] -= decrement;
-            decrement++;
-        }
-        if (++top > bottom)
-        {
-            break;
-        }
-
-        for (size_t r = top; r <= bottom; ++r)
-        {
-            matrix[r * cols + right] -= decrement;
-            decrement++;
-        }
-        if (--right < left)
-        {
-            break;
-        }
-
-        for (size_t c = right; c >= left; --c)
-        {
-            matrix[bottom * cols + c] -= decrement;
-            decrement++;
-        }
-        --bottom;
+    for (int c = left; c <= right; ++c) {
+      matrix[top * cols + c] -= decrement;
+      decrement++;
     }
+    if (++top > bottom) {
+      break;
+    }
+
+    for (int r = top; r <= bottom; ++r) {
+      matrix[r * cols + right] -= decrement;
+      decrement++;
+    }
+    if (--right < left) {
+      break;
+    }
+
+    for (int c = right; c >= left; --c) {
+      matrix[bottom * cols + c] -= decrement;
+      decrement++;
+    }
+    --bottom;
+  }
 }
 
-int numAnalysis(const char* s, int& outVal)
+int numAnalysis(const char* s)
 {
-    if (s == nullptr || s[0] == '\0')
-    {
-        return 1;
-    }
+  if (s == nullptr || s[0] == '\0') {
+    return ERROR_INVALID_FORMAT;
+  }
 
-    if (s[0] == '1' && s[1] == '\0')
-    {
-        outVal = 1;
-        return 0;
+  for (size_t i = 0; s[i] != '\0'; ++i) {
+    if (!std::isdigit(s[i])) {
+      return ERROR_INVALID_FORMAT;
     }
-    if (s[0] == '2' && s[1] == '\0')
-    {
-        outVal = 2;
-        return 0;
-    }
+  }
 
-    for (size_t i = 0; s[i] != '\0'; ++i)
-    {
-        if (s[i] < '0' || s[i] > '9')
-        {
-            return 1;
-        }
-    }
-
+  if (s[0] == '1' && s[1] == '\0') {
+    return 1;
+  }
+  if (s[0] == '2' && s[1] == '\0') {
     return 2;
+  }
+  
+  return ERROR_INVALID_VALUE;
 }
 
-int processStatic(std::ifstream& fin, std::ofstream& fout, int rows, int cols)
+int processStatic(std::ifstream& fin, std::ofstream& fout, size_t rows, size_t cols)
 {
-    int numElements = rows * cols;
+  size_t numElements = rows * cols;
 
-    if (numElements > MAX_SIZE)
-    {
-        std::cerr << "ERROR: Matrix too large for static array\n";
-        return 2;
-    }
+  if (numElements > (size_t)MAX_SIZE) {
+    std::cerr << "ERROR: Matrix too large for static array\n" << std::endl;
+    return 2;
+  }
 
-    int staticMatrix[MAX_SIZE];
+  int staticMatrix[MAX_SIZE];
 
-    if (numElements == 0)
-    {
-        fout << "0";
-        return 0;
-    }
-
-    for (size_t i = 0; i < numElements; ++i)
-    {
-        if (!(fin >> staticMatrix[i]))
-        {
-            if (fin.eof())
-            {
-                std::cerr << "ERROR: Not enough matrix elements\n";
-            }
-            else
-            {
-                std::cerr << "ERROR: Invalid matrix element\n";
-            }
-            return 2;
-        }
-    }
-
-    fout << countNonSequentialRows(staticMatrix, rows, cols);
+  if (numElements == 0) {
+    fout << "0";
     return 0;
+  }
+
+  for (size_t i = 0; i < numElements; ++i) {
+    if (!(fin >> staticMatrix[i])) {
+      if (fin.eof()) {
+        std::cerr << "ERROR: Not enough matrix elements\n" << std::endl;
+      } else {
+        std::cerr << "ERROR: Invalid matrix element\n" << std::endl;
+      }
+      return 2;
+    }
+  }
+
+  fout << countNonSequentialRows(staticMatrix, rows, cols);
+  return 0;
 }
 
-int processDynamic(std::ifstream& fin, std::ofstream& fout, int rows, int cols)
+int processDynamic(std::ifstream& fin, std::ofstream& fout, size_t rows, size_t cols)
 {
-    int numElements = rows * cols;
+  size_t numElements = rows * cols;
 
-    if (rows == 0 || cols == 0)
-    {
-        fout << "0 0";
-        return 0;
-    }
-
-    int* dynamicMatrix = new (std::nothrow) int[numElements];
-    if (dynamicMatrix == nullptr)
-    {
-        std::cerr << "ERROR: Memory not allocated for array\n";
-        return 2;
-    }
-
-    for (size_t i = 0; i < numElements; ++i)
-    {
-        if (!(fin >> dynamicMatrix[i]))
-        {
-            if (fin.eof())
-            {
-                std::cerr << "ERROR: Not enough matrix elements\n";
-            }
-            else
-            {
-                std::cerr << "ERROR: Invalid matrix element\n";
-            }
-            delete[] dynamicMatrix;
-            return 2;
-        }
-    }
-
-    spiral(dynamicMatrix, rows, cols);
-
-    fout << rows << " " << cols;
-    for (size_t i = 0; i < numElements; ++i)
-    {
-        fout << " " << dynamicMatrix[i];
-    }
-
-    delete[] dynamicMatrix;
+  if (rows == 0 || cols == 0) {
+    fout << "0 0";
     return 0;
+  }
+
+  int* dynamicMatrix = new (std::nothrow) int[numElements];
+  if (dynamicMatrix == nullptr) {
+    std::cerr << "ERROR: Memory not allocated for array\n" << std::endl;
+    return 2;
+  }
+
+  for (size_t i = 0; i < numElements; ++i) {
+    if (!(fin >> dynamicMatrix[i])) {
+      if (fin.eof()) {
+        std::cerr << "ERROR: Not enough matrix elements\n" << std::endl;
+      } else {
+        std::cerr << "ERROR: Invalid matrix element\n" << std::endl;
+      }
+      delete[] dynamicMatrix;
+      return 2;
+    }
+  }
+
+  spiral(dynamicMatrix, (int)rows, (int)cols);
+
+  fout << rows << " " << cols;
+  for (size_t i = 0; i < numElements; ++i) {
+    fout << " " << dynamicMatrix[i];
+  }
+
+  delete[] dynamicMatrix;
+  return 0;
 }
 }
