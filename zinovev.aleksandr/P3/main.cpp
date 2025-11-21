ea83c046b042b3db596dafba39f7ff1fa7438773
FShaaM <zinovevas135@gmail.com>
2025-11-21 12:28:27 +0300

    some kind of commit

--- zinovev.aleksandr/P3/main.cpp
+++ zinovev.aleksandr/P3/main.cpp
@@ -6,46 +6,32 @@
 
 int main(int argc, char** argv)
 {
-  if (argc < 4)
-  {
-    std::cerr << "Not enough arguments" << "\n";
+  if (argc < 4) {
+    std::cerr << "Not enough arguments\n";
     return 1;
-  }
-  else if (argc > 4)
-  {
-    std::cerr << "Too many arguments" << "\n";
+  } else if (argc > 4) {
+    std::cerr << "Too many arguments\n";
     return 1;
   }
 
-  int size = 0;
-  while (argv[1][size] != '\0')
-  {
-    ++size;
-  }
-
-  if (size != 1 || !std::isdigit(argv[1][0]))
-  {
-    std::cerr << "First parameter is out of range" << "\n";
+  if (argv[1][0] == '\0' || argv[1][1] != '\0' || !std::isdigit(argv[1][0])) {
+    std::cerr << "First parameter is out of range\n";
     return 1;
   }
 
-  if (argv[1][0] != '1' && argv[1][0] != '2')
-  {
-    std::cerr << "First parameter is out of range" << "\n";
+  if (argv[1][0] != '1' && argv[1][0] != '2') {
+    std::cerr << "First parameter is out of range\n";
     return 1;
   }
 
   std::ifstream input(argv[2]);
   std::ofstream output(argv[3]);
 
-  if (!input.is_open())
-  {
-    std::cerr << "ERROR: " << argv[2] << " is not open" << '\n';
+  if (!input.is_open()) {
+    std::cerr << "ERROR: " << argv[2] << " is not open\n";
     return 1;
-  }
-  else if (!output.is_open())
-  {
-    std::cerr << "ERROR: " << argv[3] << " is not open" << '\n';
+  } else if (!output.is_open()) {
+    std::cerr << "ERROR: " << argv[3] << " is not open\n";
     return 1;
   }
 
@@ -55,32 +41,24 @@ int main(int argc, char** argv)
   input >> rows;
   input >> cols;
 
-  if (!input)
-  {
-    std::cerr << "ERROR: Incorrect input data" << "\n";
+  if (!input) {
+    std::cerr << "ERROR: Incorrect input data\n";
     return 2;
   }
 
-  if (rows == 0 || cols == 0)
-  {
+  if (rows == 0 || cols == 0) {
     return 0;
   }
 
-  if (argv[1][0] == '1')
-  {
+  if (argv[1][0] == '1') {
     int array[10000] = {};
     zinovev::processArray(input, output, array, rows, cols);
-  }
-  else if (argv[1][0] == '2')
-  {
+  } else if (argv[1][0] == '2') {
     int* array = nullptr;
 
-    try
-    {
-      array = new int[rows * cols] {};
-    }
-    catch (std::bad_alloc& e)
-    {
+    try {
+      array = new int[rows * cols]{};
+    } catch (std::bad_alloc& e) {
       std::cerr << "ERROR: Memory allocation failed - " << e.what() << "\n";
       return 1;
     }
@@ -89,8 +67,7 @@ int main(int argc, char** argv)
     delete[] array;
   }
 
-  if (!input)
-  {
+  if (!input) {
     return 2;
   }
 
--- zinovev.aleksandr/P3/matrix_operations.cpp
+++ zinovev.aleksandr/P3/matrix_operations.cpp
@@ -5,19 +5,15 @@ std::istream& zinovev::readInput(std::istream& input, int* array, size_t rows, s
 {
   size_t quantity = 0;
 
-  for (size_t i = 0; i < rows * cols && input >> array[i]; ++i)
-  {
+  for (size_t i = 0; i < rows * cols && input >> array[i]; ++i) {
     ++quantity;
   }
 
-  if (!input)
-  {
-    std::cerr << "ERROR: Incorrect input" << "\n";
+  if (!input) {
+    std::cerr << "ERROR: Incorrect input\n";
     std::cerr << "Number of inputted items: " << quantity << "\n";
-  }
-  else if (rows * cols > quantity)
-  {
-    std::cerr << "ERROR: Not enough elements have been entered" << "\n";
+  } else if (rows * cols > quantity) {
+    std::cerr << "ERROR: Not enough elements have been entered\n";
     std::cerr << "Number of inputted items: " << quantity << "\n";
   }
 
@@ -30,63 +26,51 @@ void zinovev::fillSpiral(int* array, size_t rows, size_t cols)
   size_t position = (rows - 1) * cols;
   size_t total = rows * cols;
 
-  for (size_t layer = 0; number <= total; layer++)
-  {
-    for (size_t i = 0; i < (cols - 2 * layer) && number <= total; i++)
-    {
-      if (position < total)
-      {
+  for (size_t layer = 0; number <= total; layer++) {
+    for (size_t i = 0; i < (cols - 2 * layer) && number <= total; i++) {
+      if (position < total) {
         array[position] += number;
       }
       position++;
       number++;
     }
 
-    if (number > total)
-    {
+    if (number > total) {
       break;
     }
 
     position -= (cols + 1);
 
-    for (size_t i = 0; i < (rows - 2 * layer - 1) && number <= total; i++)
-    {
-      if (position < total)
-      {
+    for (size_t i = 0; i < (rows - 2 * layer - 1) && number <= total; i++) {
+      if (position < total) {
         array[position] += number;
       }
       position -= cols;
       number++;
     }
 
-    if (number > total)
-    {
+    if (number > total) {
       break;
     }
 
     position += (cols - 1);
 
-    for (size_t i = 0; i < (cols - 2 * layer - 1) && number <= total; i++)
-    {
-      if (position < total)
-      {
+    for (size_t i = 0; i < (cols - 2 * layer - 1) && number <= total; i++) {
+      if (position < total) {
         array[position] += number;
       }
       position--;
       number++;
     }
 
-    if (number > total)
-    {
+    if (number > total) {
       break;
     }
 
     position += (cols + 1);
 
-    for (size_t i = 0; i < (rows - 2 * layer - 2) && number <= total; i++)
-    {
-      if (position < total)
-      {
+    for (size_t i = 0; i < (rows - 2 * layer - 2) && number <= total; i++) {
+      if (position < total) {
         array[position] += number;
       }
       position += cols;
@@ -99,28 +83,23 @@ void zinovev::fillSpiral(int* array, size_t rows, size_t cols)
 
 int zinovev::findLongestColumn(const int* array, size_t rows, size_t cols)
 {
-  if (rows == 0)
-  {
+  if (rows == 0) {
     return 0;
   }
 
   int result = 0;
   int maxCount = 0;
 
-  for (size_t column = 0; column < cols; ++column)
-  {
+  for (size_t column = 0; column < cols; ++column) {
     int currentCount = 0;
 
-    for (size_t row = 1; row < rows; ++row)
-    {
-      if (array[cols * (row - 1) + column] == array[cols * row + column])
-      {
+    for (size_t row = 1; row < rows; ++row) {
+      if (array[cols * (row - 1) + column] == array[cols * row + column]) {
         ++currentCount;
       }
     }
 
-    if (currentCount > maxCount)
-    {
+    if (currentCount > maxCount) {
       maxCount = currentCount;
       result = column + 1;
     }
@@ -133,10 +112,8 @@ std::ostream& zinovev::writeOutput(std::ostream& output, const int* array, size_
 {
   output << result << "\n";
 
-  for (size_t i = 0; i < rows; ++i)
-  {
-    for (size_t j = 0; j < cols; ++j)
-    {
+  for (size_t i = 0; i < rows; ++i) {
+    for (size_t j = 0; j < cols; ++j) {
       output << array[i * cols + j] << " ";
     }
     output << "\n";
@@ -149,8 +126,7 @@ void zinovev::processArray(std::istream& input, std::ostream& output, int* array
 {
   readInput(input, array, rows, cols);
 
-  if (!input)
-  {
+  if (!input) {
     return;
   }
 
