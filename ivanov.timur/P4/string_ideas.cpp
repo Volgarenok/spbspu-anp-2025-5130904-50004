c21757edc1c866eb5ac129d8dd790d0c9c7c73bf
isaacgreenfield <ivtim708@gmail.com>
2025-11-21 23:50:38 +0300

    Finalized string_ideas

--- ivanov.timur/P4/string_ideas.cpp
+++ ivanov.timur/P4/string_ideas.cpp
@@ -3,18 +3,25 @@
 #include <cctype>
 
 bool ivanov::del_lat(char* content, size_t size) {
-  char* tmp = new char[100000];
-  size_t c = 0;
-  for (size_t i = 0; i < size; ++i) {
-    if (!isalpha(content[i])) {
-      tmp[i - c] = content[i];
-    } else {
-      c++;
+  try {
+    char* tmp = new char[100000];
+    size_t c = 0;
+    for (size_t i = 0; i < size; ++i) {
+      if (!isalpha(content[i])) {
+        tmp[i - c] = content[i];
+      } else {
+        c++;
+      }
     }
+    delete[] content;
+    content = tmp;
+    size-=c;
+    return true;
   }
-  delete[] content;
-  content = tmp;
-  size-=c;
+  catch (...) {
+    delete[] content;
+    return false;
+  }
 }
 void ivanov::output(const char* content, size_t size) {
   for (size_t i = 0; i < size; ++i) {
@@ -23,30 +30,77 @@ void ivanov::output(const char* content, size_t size) {
   std::cout << "\n";
 }
 bool ivanov::spc_rmv(char *content, size_t size) {
-  char* tmp = new char[100000];
-  bool flag = false;
-  size_t c = 0;
+  try {
+    char* tmp = new char[100000];
+    bool flag = false;
+    size_t c = 0;
+    for (size_t i = 0; i < size; ++i) {
+      if ((!flag || content[i - 1] == ' ') && content[i] == ' ') {
+        tmp[i - c] = content[i];
+        flag = true;
+      } else {
+        c++;
+      }
+    }
+    delete[] content;
+    size-=c;
+    return true;
+  }
+  catch (...) {
+    delete[] content;
+    return false;
+  }
+}
+bool ivanov::merge(char* content1, char* content2, size_t size1, size_t size2) {
+  try {
+    char* tmp = new char[100000];
+    for (size_t i = 0; i < size1; ++i) {
+      tmp[i] = content1[i];
+    }
+    for (size_t i = size1; i < size1 + size2; ++i) {
+      tmp[i] = content2[i - size1];
+    }
+    delete[] content1;
+    delete[] content2;
+    content1 = tmp;
+    size1+=size2;
+    return true;
+  }
+  catch (...) {
+    delete[] content1;
+    delete[] content2;
+    return false;
+  }
+}
+bool ivanov::get_find(char* content, size_t size) {
+  try {
+    char* tmp = new char[100000];
+    size_t l = 0;
+    char x = 'a';
+    char z = 'A';
+    for (size_t c = 0; c < 26; ++c) {
+      if (find(x, content, size) || find(z, content, size)) {
+        tmp[l] = x;
+        l++;
+      }
+      x++;
+      z++;
+    }
+    delete[] content;
+    content = tmp;
+    size = l;
+    return true;
+  }
+  catch (...) {
+    delete[] content;
+    return false;
+  }
+}
+bool ivanov::find(char symbol, const char* content, size_t size) {
   for (size_t i = 0; i < size; ++i) {
-    if ((!flag || content[i - 1] == ' ') && content[i] == ' ') {
-      tmp[i - c] = content[i];
-      flag = true;
-    } else {
-      c++;
+    if (content[i] == symbol) {
+      return true;
     }
   }
-  delete[] content;
-  size-=c;
-}
-bool ivanov::merge(char *content1, char *content2, size_t size1, size_t size2) {
-  char* tmp = new char[100000];
-  for (size_t i = 0; i < size1; ++i) {
-    tmp[i] = content1[i];
-  }
-  for (size_t i = size1; i < size1 + size2; ++i) {
-    tmp[i] = content2[i - size1];
-  }
-  delete[] content1;
-  delete[] content2;
-  content1 = tmp;
-  size1+=size2;
+  return false;
 }
--- ivanov.timur/P4/string_ideas.h
+++ ivanov.timur/P4/string_ideas.h
@@ -5,5 +5,7 @@ namespace ivanov {
   bool del_lat(char* content, size_t size);
   void output(const char* content, size_t size);
   bool merge(char* content1, char* content2, size_t size1, size_t size2);
+  bool get_find(char* content, size_t size);
+  bool find(char symbol, const char* content, size_t size);
 }
 #endif
