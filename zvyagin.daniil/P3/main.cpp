91ca11dc29b322066afe9a7fde431bbcd0fad96e
zvxgich <dan011807@gmail.com>
2025-11-21 18:04:23 +0300

    add translation units

--- zvyagin.daniil/P3/main.cpp
+++ zvyagin.daniil/P3/main.cpp
@@ -1,70 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
-
-namespace zvyagin
-{
-
-size_t count_columns_no_same_adjacent(const int* data, size_t rows, size_t cols)
-{
-    if (cols == 0) {
-        return 0;
-    }
-    if (rows < 2) {
-        return cols;
-    }
-    unsigned count = 0;
-    for (size_t j = 0; j < cols; ++j)
-    {
-        bool has_same = false;
-        for (size_t i = 0; i < rows - 1; ++i)
-        {
-            if (data[i * cols + j] == data[(i + 1) * cols + j])
-            {
-                has_same = true;
-                break;
-            }
-        }
-        if (!has_same) {
-            ++count;
-        }
-    }
-    return count;
-}
-
-size_t count_local_maxima(const int* data, size_t rows, size_t cols)
-{
-    if (rows < 3 || cols < 3) {
-        return 0;
-    }
-    unsigned count = 0;
-    for (size_t i = 1; i < rows - 1; ++i)
-    {
-        for (size_t j = 1; j < cols - 1; ++j)
-        {
-            int cur = data[i * cols + j];
-            bool is_max = true;
-            for (int di = -1; di <= 1 && is_max; ++di)
-            {
-                for (int dj = -1; dj <= 1; ++dj)
-                {
-                    if (di == 0 && dj == 0) {
-                        continue;
-                    }
-                    if (cur <= data[(i + di) * cols + (j + dj)]) {
-                        is_max = false;
-                        break;
-                    }
-                }
-            }
-            if (is_max) {
-                ++count;
-            }
-        }
-    }
-    return count;
-}
-} // namespace zvyagin
+#include "functions.hpp"
 
 int main(int argc, char** argv)
 {
