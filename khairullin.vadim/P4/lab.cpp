d6b4a82677053ba0654ebcd1c94ee84deca761a3
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-21 22:48:21 +0300

    Initial commit

---
+++ khairullin.vadim/P4/lab.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+int main()
+{
+  size_t size1 = 0;
+  char * str1 = new char [size1];
+  size_t size2 = 11;
+  char str2[size2] = {'F', 'o', 'r', 'e', 'x', 'a', 'm', 'p', 'l', 'e', '\0')}
+}
