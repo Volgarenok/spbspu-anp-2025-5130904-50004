2678b08e5e4e27f7ccec9aebbcd572c1300b787e
knopkloop <hruslan2007@gmail.com>
2025-11-22 02:50:18 +0300

    added DIF_LAT function realization

--- haliullin.ruslan/P4/main.cpp
+++ haliullin.ruslan/P4/main.cpp
@@ -6,23 +6,27 @@
 namespace haliullin
 {
   char *getline(std::istream &in, size_t &size);
-  size_t DIF_LAT(const char *arr, size_t size);
-  char *RMV_VOW(char *new_arr, const char *arr, size_t size);
+  size_t DIF_LAT(const char *arr, size_t str_size);
+  char *RMV_VOW(char *new_arr, const char *arr, size_t str_size);
   void writeString(const char *arr, size_t str_size);
 }
 int main()
 {
+  namespace hal = haliullin;
   char *arr = nullptr;
   size_t size = 0;
+  size_t count = 0;
 
-  arr = haliullin::getline(std::cin, size);
+  arr = hal::getline(std::cin, size);
   if (arr == nullptr)
   {
     std::cerr << "Memory allocation error" << "\n";
     return 1;
   }
   size_t str_size = size - 1;
-  haliullin::writeString(arr, str_size);
+  hal::writeString(arr, str_size);
+  count = hal::DIF_LAT(arr, str_size);
+  std::cout << count << "\n";
   free(arr);
   return 0;
 }
@@ -75,13 +79,33 @@ char *haliullin::getline(std::istream &in, size_t &size)
       arr = temp;
       ++size;
     }
-    arr[size - 1] = 0;
+    arr[size] = 0;
   }
   if (is_skipws)
   {
     in >> std::skipws;
   }
   return arr;
+}
+
+size_t haliullin::DIF_LAT(const char *arr, size_t str_size)
+{
+  size_t count = 0;
+  size_t ind = 0;
+  bool info[26] = {};
+  for (size_t i = 0; i < str_size; ++i)
+  {
+    if ((arr[i] >= 'a' && arr[i] <= 'z') || (arr[i] >= 'A' && arr[i] <= 'Z'))
+    {
+      ind = char(std::tolower(arr[i])) - 'a';
+      if (!info[ind])
+      {
+        info[ind] = true;
+        ++count;
+      }
+    }
+  }
+  return count;
 }
 
 void haliullin::writeString(const char *arr, size_t str_size)
