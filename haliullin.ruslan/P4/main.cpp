bd95fef3951cf9bf28b28b8425404ba54774cc12
knopkloop <hruslan2007@gmail.com>
2025-11-22 01:18:40 +0300

    added getline function

--- haliullin.ruslan/P4/main.cpp
+++ haliullin.ruslan/P4/main.cpp
@@ -5,56 +5,61 @@
 #include <cctype>
 namespace haliullin
 {
-  char *getline(std::istream &in, size_t &s);
+  char *getline(std::istream &in, size_t &size);
   size_t DIF_LAT(const char *arr, size_t size);
   char *RMV_VOW(char *new_arr, const char *arr, size_t size);
   void writeString(const char *arr, size_t size);
 }
 int main()
 {
-  char symb = '\0';
-  size_t size = 0;
   char *arr = nullptr;
-  std::cin >> symb;
-  ++size;
-  if (!std::cin)
-  {
-    std::cerr << "Unexpected input" << "\n";
-    return 2;
-  }
-  arr = static_cast < char* >(malloc((size) * sizeof(char)));
+  size_t size = 0;
+  
+  arr = haliullin::getline(std::cin, size);
   if (arr == nullptr)
   {
     std::cerr << "Memory allocation error" << "\n";
     return 1;
   }
-  arr[0] = symb;
-  while (std::cin >> symb && symb != '\0')
+  haliullin::writeString(arr, size);
+  std::cout << size << "\n";
+  free(arr);
+  return 0;
+}
+
+char *haliullin::getline(std::istream &in, size_t &size)
+{
+  char symb = '\0';
+  char *arr = nullptr;
+  size = 0;
+  
+  in >> symb;
+  ++size;
+  arr = static_cast < char* >(malloc(sizeof(char)));
+  if (arr == nullptr)
   {
-    if(!std::cin)
-    {
-      std::cerr << "Unexpected input" << "\n";
-      return 2;
-    }
+    return nullptr;
+  }
+  arr[0] = symb;
+  while (in >> symb && symb != '\0')
+  {
     char *temp = nullptr;
     temp = static_cast < char* >(malloc((size + 1) * sizeof(char)));
     if (temp == nullptr)
     {
-      std::cerr << "Memory allocation error" << "\n";
-      return 1;
+      return nullptr;
     }
     for (size_t i = 0; i < size; ++i)
     {
       temp[i] = arr[i];
     }
     temp[size] = symb;
+    
     free(arr);
     arr = temp;
     ++size;
   }
-  haliullin::writeString(arr, size);
-  free(arr);
-  return 0;
+  return arr;
 }
 
 void haliullin::writeString(const char *arr, size_t size)
