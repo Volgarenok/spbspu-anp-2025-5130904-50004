360cd666b6d42a114570e753ff94abfea55e4cae
isaacgreenfield <ivtim708@gmail.com>
2025-11-21 23:20:40 +0300

    Signed functions concept, added input

--- ivanov.timur/P4/main.cpp
+++ ivanov.timur/P4/main.cpp
@@ -1,3 +1,23 @@
+#include <iostream>
+#include "string_ideas.h"
+void get_line(std::istream & in, char* data, size_t size) {
+  bool is_skipws = in.flags() & std::ios_base::skipws;
+  if (is_skipws) {
+    in >> std::noskipws;
+  }
+  char tmp;
+  in >> tmp;
+  while (tmp != '\n') {
+    data[size] = tmp;
+    size++;
+    in >> tmp;
+  }
+  data[size] = 0;
+  size++;
+  if (is_skipws) {
+    in >> std::skipws;
+  }
+}
 int main() {
   return 0;
 }
--- ivanov.timur/P4/string_ideas.h
+++ ivanov.timur/P4/string_ideas.h
@@ -2,7 +2,11 @@
 #define STRING_IDEAS_H
 
 namespace ivanov {
-
+  void spc_rmv(char* content, size_t size);
+  void rmv(char* conent, size_t size, size_t id);
+  void del_lat(char* content, size_t size);
+  void output(char* content);
+  char* merge(char* content1, char* content2);
 }
 
 #endif
