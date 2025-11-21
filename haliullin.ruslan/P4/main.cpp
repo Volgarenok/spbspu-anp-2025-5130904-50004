524cf9ae52ab0f7239cc670b3760c68cf55be59c
knopkloop <hruslan2007@gmail.com>
2025-11-21 22:32:33 +0300

    added functions declarations in personal namespace

--- haliullin.ruslan/P4/main.cpp
+++ haliullin.ruslan/P4/main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include <iomanip>
+#include <cstdlib>
+#include <fstream>
+namespace haliullin
+{
+  char *getline(std::istream &in, size_t &s);
+  size_t DIF_LAT(const char *arr, size_t size);
+  char *RMV_VOW(char *new_arr, const char *arr, size_t size);
+  void writeString(const char *arr, size_t size);
+}
 int main()
 {
 }
