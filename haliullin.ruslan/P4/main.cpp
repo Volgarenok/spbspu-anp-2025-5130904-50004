fbeb36d0adf4f7e3a0dd7e77a66d5386331ddfc0
knopkloop <hruslan2007@gmail.com>
2025-11-22 00:14:05 +0300

    added input sequence skelet

--- haliullin.ruslan/P4/main.cpp
+++ haliullin.ruslan/P4/main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <cstdlib>
 #include <fstream>
+#include <cctype>
 namespace haliullin
 {
   char *getline(std::istream &in, size_t &s);
@@ -11,4 +12,44 @@ namespace haliullin
 }
 int main()
 {
+  char symb = '\0';
+  size_t size = 0;
+  char *arr = nullptr;
+  std::cin >> symb;
+  ++size;
+  if (!std::cin)
+  {
+    std::cerr << "Unexpexted input" << "\n";
+    return 2;
+  }
+  arr = static_cast < char* >(malloc((size) * sizeof(char)));
+  if (arr == nullptr)
+  {
+    std::cerr << "Memory allocation error" << "\n";
+    return 1;
+  }
+  arr[0] = symb;
+  while (std::cin >> symb && symb != '\0')
+  {
+    char *temp = nullptr;
+    temp = static_cast < char* >(malloc((size + 1) * sizeof(char)));
+    if (temp == nullptr)
+    {
+      std::cerr << "Memory allocation error" << "\n";
+      return 1;
+    }
+    for (size_t i = 0; i < size; ++i)
+    {
+      temp[i] = arr[i];
+    }
+    temp[size] = symb;
+    free(arr);
+    arr = temp;
+    ++size;
+  }
+  for(size_t i = 0; i < size; ++i)
+  {
+    std::cout << arr[i] << "\n";
+  }
+  return 0;
 }
