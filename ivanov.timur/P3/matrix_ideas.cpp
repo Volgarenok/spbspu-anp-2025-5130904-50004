0bc5ee83ef7d0957022dabc339e4d4c80609c5eb
isaacgreenfield <ivtim708@gmail.com>
2025-11-21 12:00:26 +0300

    2.3 round of fixes

--- ivanov.timur/P3/matrix_ideas.cpp
+++ ivanov.timur/P3/matrix_ideas.cpp
@@ -5,10 +5,10 @@
 
 void ivanov::fll_inc_wav(int* mtr, size_t rows, size_t cols)
 {
-  int lvs = (std::min(rows, cols) + 1) / 2;
+  size_t lvs = (std::min(rows, cols) + 1) / 2;
   for (size_t lv = 0; lv < lvs; lv++)
   {
-    int inc = lv + 1;
+    size_t inc = lv + 1;
     int* top_row = mtr + lv * cols;
     int* bottom_row = mtr + (rows - lv - 1) * cols;
     for (size_t j = lv; j < cols - lv; j++)
@@ -26,11 +26,7 @@ void ivanov::fll_inc_wav(int* mtr, size_t rows, size_t cols)
 }
 int ivanov::max_sum_mdg(const int* matrix, size_t rows, size_t cols)
 {
-  if (rows == 0 || cols == 0)
-  {
-    return 0;
-  }
-  int size_sums = rows + cols - 1;
+  size_t size_sums = rows + cols - 1;
   if (size_sums > 10000)
   {
     return 0;
@@ -40,12 +36,12 @@ int ivanov::max_sum_mdg(const int* matrix, size_t rows, size_t cols)
   {
     for (size_t j = 0; j < cols; j++)
     {
-      int diagonal_index = i + j;
+      size_t diagonal_index = i + j;
       sums[diagonal_index] += matrix[i * cols + j];
     }
   }
-  int main_anti_diagonal = rows - 1;
-  int max_sum = 0;
+  size_t main_anti_diagonal = rows - 1;
+  size_t max_sum = 0;
   bool found_valid = false;
   for (size_t s = 0; s < size_sums; s++)
   {
