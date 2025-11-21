63227cdc8e26d4a6f87c943b57be336ac5c53490
isaacgreenfield <ivtim708@gmail.com>
2025-11-21 23:38:33 +0300

    Cleaned string_ideas

--- ivanov.timur/P4/string_ideas.cpp
+++ ivanov.timur/P4/string_ideas.cpp
@@ -1,2 +1,52 @@
-
 #include "string_ideas.h"
+#include <iostream>
+#include <cctype>
+
+bool ivanov::del_lat(char* content, size_t size) {
+  char* tmp = new char[100000];
+  size_t c = 0;
+  for (size_t i = 0; i < size; ++i) {
+    if (!isalpha(content[i])) {
+      tmp[i - c] = content[i];
+    } else {
+      c++;
+    }
+  }
+  delete[] content;
+  content = tmp;
+  size-=c;
+}
+void ivanov::output(const char* content, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    std::cout << content[i];
+  }
+  std::cout << "\n";
+}
+bool ivanov::spc_rmv(char *content, size_t size) {
+  char* tmp = new char[100000];
+  bool flag = false;
+  size_t c = 0;
+  for (size_t i = 0; i < size; ++i) {
+    if ((!flag || content[i - 1] == ' ') && content[i] == ' ') {
+      tmp[i - c] = content[i];
+      flag = true;
+    } else {
+      c++;
+    }
+  }
+  delete[] content;
+  size-=c;
+}
+bool ivanov::merge(char *content1, char *content2, size_t size1, size_t size2) {
+  char* tmp = new char[100000];
+  for (size_t i = 0; i < size1; ++i) {
+    tmp[i] = content1[i];
+  }
+  for (size_t i = size1; i < size1 + size2; ++i) {
+    tmp[i] = content2[i - size1];
+  }
+  delete[] content1;
+  delete[] content2;
+  content1 = tmp;
+  size1+=size2;
+}
--- ivanov.timur/P4/string_ideas.h
+++ ivanov.timur/P4/string_ideas.h
@@ -1,12 +1,9 @@
 #ifndef STRING_IDEAS_H
 #define STRING_IDEAS_H
-
 namespace ivanov {
-  void spc_rmv(char* content, size_t size);
-  void rmv(char* conent, size_t size, size_t id);
-  void del_lat(char* content, size_t size);
-  void output(char* content);
-  char* merge(char* content1, char* content2);
+  bool spc_rmv(char* content, size_t size);
+  bool del_lat(char* content, size_t size);
+  void output(const char* content, size_t size);
+  bool merge(char* content1, char* content2, size_t size1, size_t size2);
 }
-
 #endif
