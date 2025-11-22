1cbb95520dc8b4e4495b993d3071b4a450378cf5
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-22 21:15:12 +0300

    create header guard and obiavila functions

---
+++ kuchukbaeva.alyona/P4/string_functions.hpp
@@ -0,0 +1,14 @@
+#ifndef STRING_FUNCTIONS_HPP
+#define STRING_FUNCTIONS_HPP
+#include <iostream>
+
+namespace kuchukbaeva
+{
+  size_t strLen(const char* str);
+  int isVowel(char c);
+  char* rmvVow(const char* src, char* dest) noexcept;
+  int repDgt(const char* str) noexcept;
+  char* readStr(std::istream& input);
+}
+
+#endif
