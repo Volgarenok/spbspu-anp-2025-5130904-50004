70fa19f1e80f1bc7796fec723ded1f36d1559e4d
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-22 17:31:02 +0300

    I added function to look for unique elements in the string

---
+++ khairullin.vadim/P4/unique_str.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+namespace khairullin
+{
+  void khairullin::unique_str(char * str1; char * str2; size_t size1; size_t size2)
+  {
+    bool flag = false;
+    for (size_t i = 0; i < size1; ++i)
+    {
+      for (size_t j = 0; j < size2; ++j)
+      {
+        if (str2[j] == str1[i])
+        {
+          flag = true;
+        }
+      }
+      if (!flag and str1[i] != ' ')
+      {
+        std::cout << str1[i];
+      }
+      flag = false;
+    }
+    for (size_t i = 0; i < size2; ++i)
+    {
+      for (size_t j = 0; j < size1; ++j)
+      {
+        if (str1[j] == str2[i])
+        {
+          flag = true;
+        }
+      }
+      if (!flag and str2[i] != ' ')
+      {
+        std::cout << str2[i];
+      }
+      flag = false;
+    }
+    std::cout << "\n";
+  }
+}
