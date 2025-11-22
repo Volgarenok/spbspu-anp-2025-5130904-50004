0b034df23b09da4d6910ff7b8fa122c0fb23b273
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-22 15:57:11 +0300

    I divided program into the translation units

---
+++ khairullin.vadim/P4/khairullin.hpp
@@ -0,0 +1,7 @@
+#ifndef KHAIRULLIN_HPP
+#define KHAIRULLIN_HPP
+namespace khairullin
+{
+  size_t getline(std::istream & input, char *& data, size_t size);
+}
+#endif
--- khairullin.vadim/P4/main.cpp
+++ khairullin.vadim/P4/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
-#include <iomanip>
+#include "khairullin.hpp"
+ 
 
 int main()
 {
   size_t size1 = 0;
   char * str1 = new char [size1];
+  size1 = khairullin::getline(std::cin, str1, size1);
   size_t size2 = 11;
   char str2[size2] = {'F', 'o', 'r', 'e', 'x', 'a', 'm', 'p', 'l', 'e', '\0')}
 }
