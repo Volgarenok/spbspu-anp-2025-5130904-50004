2f9172a18927d405fd4f65d15ce393f6f4fe3243
maxchadin <maxchadin080418@gmail.com>
2025-11-22 23:15:19 +0300

    add matrix_actions.cpp

---
+++ chadin.maxim/P3/matrix_actions.cpp
@@ -0,0 +1,26 @@
+#include "matrix_actions.hpp"
+#include <istream>
+#include <ostream>
+
+namespace chadin
+{
+int* readMatrix(std::istream& in, int& rows, int& cols)
+{
+}
+
+void deleteMatrix(int* matrix)
+{
+}
+
+int countNonZeroDiagonals(const int* matrix, int rows, int cols)
+{
+}
+
+void transformSpiralFromBottomLeft(int* matrix, int rows, int cols)
+{
+}
+
+void writeMatrix(std::ostream& out, const int* matrix, int rows, int cols)
+{
+}
+}
