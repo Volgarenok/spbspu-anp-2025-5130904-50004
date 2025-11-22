738eed42353ced8a5750180bdfa72abdd1e49bc2
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-22 23:26:21 +0300

    I repaired main

--- khairullin.vadim/P4/main.cpp
+++ khairullin.vadim/P4/main.cpp
@@ -3,24 +3,20 @@
 
 int main()
 {
+  char * str1 = nullptr;
   try
   {
     size_t size1 = 0;
-    char * str1 = new char [size1];
     size1 = khairullin::getline(std::cin, str1, size1);
     const size_t size2 = 11;
-    if (size1 == 0)
-    {
-      std::cerr << "No string\n";
-      return 1;
-    }
     const char str2[size2] = {'F', 'o', 'r', 'e', 'x', 'a', 'm', 'p', 'l', 'e', '\0'};
     khairullin::unique_str(str1, str2, size1, size2);
     khairullin::delete_vowels(str1, size1);
     delete [] str1;
   }
-  catch (std::bad_alloc & msg)
+  catch (std::exception & msg)
   {
+    delete [] str1;
     std::cerr << msg.what();
     return 1;
   }
