f345dcd643f98a02bbb7d80efee6662aaf6feac2
SoundwaveAI <ghost10.town10000@gmail.com>
2025-11-22 21:34:13 +0300

    create vivod and podpravila cg

--- kuchukbaeva.alyona/P4/main.cpp
+++ kuchukbaeva.alyona/P4/main.cpp
@@ -1,3 +1,26 @@
+#include "string_functions.hpp"
+#include <iostream>
+#include <new>
+
 int main()
-{}
+{
+  try
+  {
+    char* input_str = input_str = kuchukbaeva::readStr(std::cin);
+    const size_t len = kuchukbaeva::strLen(input_str);
+    char* res_str = new char[len + 1];
+    kuchukbaeva::rmvVow(input_str, res_str);
+    std::cout << res_str << "\n";
+    int repeatdig = kuchukbaeva::repDgt(input_str);
+    std::cout << repeatdig << "\n";
+    delete[] input_str;
+    delete[] res_str;
+    return 0;
+  }
+  catch (const std::bad_alloc& e)
+  {
+    std::cerr << "Memory allocation failed (" << e.what() << ")" << "\n";
+    return 1;
+  }
+}
 
