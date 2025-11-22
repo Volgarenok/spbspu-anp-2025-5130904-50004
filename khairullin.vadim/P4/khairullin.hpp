2e6cac4f0f61fdbc269b835272c5a6135b4e55cb
BinaryKenny <khairullinvadim2006@gmail.com>
2025-11-22 17:33:06 +0300

    I added the headline of function into the main.cpp and khairullin.hpp

--- khairullin.vadim/P4/khairullin.hpp
+++ khairullin.vadim/P4/khairullin.hpp
@@ -3,5 +3,6 @@
 namespace khairullin
 {
   size_t getline(std::istream & input, char *& data, size_t size);
+  void unique_str(char * str1; char * str2; size_t size1; size_t size2);
 }
 #endif
--- khairullin.vadim/P4/main.cpp
+++ khairullin.vadim/P4/main.cpp
@@ -9,4 +9,5 @@ int main()
   size1 = khairullin::getline(std::cin, str1, size1);
   size_t size2 = 11;
   char str2[size2] = {'F', 'o', 'r', 'e', 'x', 'a', 'm', 'p', 'l', 'e', '\0')}
+  khairullin::unique_str(str1, str2, size1, size2);
 }
