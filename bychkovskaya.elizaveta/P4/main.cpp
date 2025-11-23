e21e1fe46c14953012bb2081b4f585d0219345fe
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-11-23 20:37:28 +0300

    fix getline + add excsnd

--- bychkovskaya.elizaveta/P4/main.cpp
+++ bychkovskaya.elizaveta/P4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cctype>
 #include <iomanip>
+#include <fstream>
 
 char * create(size_t size) 
 {
@@ -11,25 +12,28 @@ char * create(size_t size)
 
 char * getline(std::istream & in, size_t & size)
 {
+  char elem = '\0';
   size = 1; 
   bool is_skipws = in.flags() & std::ios_base::skipws;
   if (is_skipws) {
     in >> std::noskipws;
   }
   char * data = create(size + 1);
-  in >> data[0];
+  if (in >> elem && elem != '\n') {
+    data[0] = elem;
+  }
   ++size; 
-  while (in) {
+  while (in >> elem && elem != '\n') {
     char * tmp = create(size + 1); 
     for (size_t i = 0; i < size - 1; ++i) {    
       tmp[i] = data[i]; 
     }
-    in >> tmp[size - 1];
-    if (in)  {   
+    tmp[size - 1] = elem;
+    // if (in >> elem && elem != '\0' && elem != '\n')  {   
       free(data);
       data = tmp;
       ++size;
-    }  
+    // }  
   }
   data[size-1] = '\0';  
   if (is_skipws) {
@@ -46,17 +50,38 @@ char * getline(std::istream & in, size_t & size)
 //взять первую строку и каждый ее символ сравнить с каждым символом второй строки (два фора)
 //в новый массив длина которого равна длине первой строки записать те символы который не встречаются во второй строке
 
-// excsnd(const char * str1, const char * str2,) 
-// {
-
-// }
+void excsnd(const char * str1, const char * str2, char * result) 
+{
+  size_t countRepeat = 0;
+  size_t k = 0;
+  for (size_t i = 0; str1[i] != '\0'; ++i) {
+    countRepeat = 0;
+    for (size_t j = 0; str2[j] != '\0'; ++j) {
+      if (str1[i] == str2[j]) {
+        ++countRepeat;
+      }
+    }
+    if (countRepeat == 0) {
+      result[k] = str1[i];
+      ++k;
+    }
+  }
+}
 
 int main()
 {
-  size_t size = 0;
-  char * data = getline(std::cin, size);
+  size_t size1 = 0;
+  size_t size2 = 0;
+  std::cout << "\n" << "Введите первую строку" << "\n";
+  char * str1 = getline(std::cin, size1);
+  std::cout << "\n" << size1 << "\n";
+  std::cout << "\n" << "Введите вторую строку" << "\n";
+  char * str2 = getline(std::cin, size2);
+  std::cout << "\n" << size2 << "\n";
+  char * result = create(size1);
+  excsnd(str1, str2, result);
   std::cout << "\n";
-  std::cout << size << "\n";
-  std::cout << data << "\n";
-  free(data);
+  std::cout << result << "\n";
+  free(str1);
+  free(str2);
 }
