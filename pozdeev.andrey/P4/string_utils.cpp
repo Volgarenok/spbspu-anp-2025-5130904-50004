aae2c42c4329f6d3165b450d93b22e647cb4bb3b
ryouta25 <avp22.07@mail.ru>
2025-11-23 21:05:36 +0300

    Added function for SPC-RMV

---
+++ pozdeev.andrey/P4/string_utils.cpp
@@ -0,0 +1,36 @@
+#include "string_utils.hpp"
+#include <cctype>
+#include <cstddef>
+
+char * pozdeev::removeExtraSpaces(char * destination, const char * source)
+{
+  size_t readIndex = 0;
+  size_t writeIndex = 0;
+  int isSpaceFound = 0;
+
+  while (source[readIndex] != '\0') {
+    if (std::isspace(source[readIndex]) != 0) {
+      if (writeIndex > 0) {
+        if (isSpaceFound == 0) {
+          destination[writeIndex] = ' ';
+          writeIndex = writeIndex + 1;
+          isSpaceFound = 1;
+        }
+      }
+    } else {
+      destination[writeIndex] = source[readIndex];
+      writeIndex = writeIndex + 1;
+      isSpaceFound = 0;
+    }
+    readIndex = readIndex + 1;
+  }
+
+  if (writeIndex > 0) {
+    if (std::isspace(destination[writeIndex - 1]) != 0) {
+      writeIndex = writeIndex - 1;
+    }
+  }
+
+  destination[writeIndex] = '\0';
+  return destination;
+}
