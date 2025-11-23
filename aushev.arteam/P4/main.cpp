cda21089e18eabb22a35cba69587b01c9d4f507d
root <root@Asus.localdomain>
2025-11-23 18:24:53 +0300

    lab4: add dynamic input reading with new[]

--- aushev.arteam/P4/main.cpp
+++ aushev.arteam/P4/main.cpp
@@ -1,3 +1,34 @@
-int main() {
+#include "string_utils.hpp"
+#include <cstdio>
+#include <new>
 
+int main() {
+    int ch;
+    size_t len = 0;
+    size_t cap = 64;
+    char* buf = new (std::nothrow) char[cap];
+    if (!buf) {
+        std::fputs("Allocation error\n", stderr);
+        return 1;
+    }
+
+    while ((ch = std::getchar()) != EOF && ch != '\n') {
+        if (len + 1 >= cap) {
+            cap *= 2;
+            char* tmp = new (std::nothrow) char[cap];
+            if (!tmp) {
+                delete[] buf;
+                std::fputs("Allocation error\n", stderr);
+                return 1;
+            }
+            for (size_t i = 0; i <= len; ++i) tmp[i] = buf[i];
+            delete[] buf;
+            buf = tmp;
+        }
+        buf[len++] = static_cast<char>(ch);
+    }
+    buf[len] = '\0';
+
+    delete[] buf;
+    return 0;
 }
