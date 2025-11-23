75541f8fd3d6fa2c5b705fdb6bc9e32550b83433
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-23 18:57:31 +0300

    Add structure

--- ivantsova.ekaterina/P3/P4/main.cpp
+++ ivantsova.ekaterina/P3/P4/main.cpp
@@ -1,4 +1,29 @@
+#include <iostream>
+
+namespace ivantsova
+{
+  char * readLine(std::istream & in, size_t & size);
+  char * interleaveStrings(const char * str1, const char * str2, size_t size1, size_t & result_size);
+  char * addDigits(const char * str1, const char * str2, size_t size1, size_t & result_size);
+  void printString(std::ostream & out, const char * str, size_t size);
+}
+
+char * ivantsova::readLine(std::istream & in, size_t & size)
+{
+}
+
+char * ivantsova::interleaveStrings(const char * str1, const char * str2, size_t size1, size_t & result_size)
+{
+}
+
+char * ivantsova::addDigits(const char * str1, const char * str2, size_t size1, size_t & result_size)
+{
+}
+
+void ivantsova::printString(std::ostream & out, const char * str, size_t size)
+{
+}
+
 int main()
 {
-
 }
