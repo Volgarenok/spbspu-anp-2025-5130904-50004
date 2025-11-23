093ab57705b7b55310e29a5748f947a9ed79d81b
root <root@Asus.localdomain>
2025-11-23 17:55:14 +0300

    lab4: implement has_sam logic

--- aushev.arteam/P4/string_utils.cpp
+++ aushev.arteam/P4/string_utils.cpp
@@ -1,1 +1,20 @@
 #include "string_utils.hpp"
+#include <cstddef>
+
+namespace aushev {
+
+int has_sam(const char* s1, const char* s2) {
+    if (!s1 || !s2) return 0;
+    const char* p1 = s1;
+    while (*p1) {
+        const char* p2 = s2;
+        while (*p2) {
+            if (*p1 == *p2) return 1;
+            ++p2;
+        }
+        ++p1;
+    }
+    return 0;
+}
+
+}
