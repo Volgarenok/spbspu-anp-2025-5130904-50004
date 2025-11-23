1c0ad914777809edac98b702a83e47feff3e76da
ryouta25 <avp22.07@mail.ru>
2025-11-23 21:21:44 +0300

    Added malloc memory

--- pozdeev.andrey/P4/main.cpp
+++ pozdeev.andrey/P4/main.cpp
@@ -9,4 +9,13 @@ namespace {
 }
 
 int main()
-{}
+{
+  try {
+    size_t currentCapacity = initialBufferSize;
+    size_t currentSize = 0;
+
+    char * inputString = static_cast <char *> (std::malloc(currentCapacity * sizeof(char)));
+    if (inputString == nullptr) {
+      throw std::bad_alloc();
+    }
+}
