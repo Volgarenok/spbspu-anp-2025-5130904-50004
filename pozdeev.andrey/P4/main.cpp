e7ea4b53be51983c74f0230bc42893f41f97699e
ryouta25 <avp22.07@mail.ru>
2025-11-23 21:17:28 +0300

    Added Skeleton for main.cpp

---
+++ pozdeev.andrey/P4/main.cpp
@@ -0,0 +1,12 @@
+#include "string_utils.hpp"
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+
+namespace {
+  constexpr size_t initialBufferSize = 16;
+  constexpr size_t resizeFactor = 2;
+}
+
+int main()
+{}
