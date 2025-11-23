092e914b62a2a0c627b6d96f22d7f132e803a7e3
ekaterinaivanc <kataivancova7@gmail.com>
2025-11-23 19:35:00 +0300

    Add addDigits

--- ivantsova.ekaterina/P3/P4/main.cpp
+++ ivantsova.ekaterina/P3/P4/main.cpp
@@ -87,6 +87,39 @@ char * ivantsova::interleaveStrings(const char * str1, const char * str2, size_t
 
 char * ivantsova::addDigits(const char * str1, const char * str2, size_t size1, size_t & result_size)
 {
+  size_t size2 = 0;
+  while (str2[size2] != '\0')
+  {
+    ++size2;
+  }
+  size_t digit_count = 0;
+  for (size_t i = 0; i < size2; ++i)
+  {
+    if (std::isdigit(static_cast<unsigned char>(str2[i])))
+    {
+      ++digit_count;
+    }
+  }
+  result_size = size1 + digit_count;
+  char * result = static_cast<char *>(malloc((result_size + 1) * sizeof(char)));
+  if (result == nullptr)
+  {
+    return result;
+  }
+  for (size_t i = 0; i < size1; ++i)
+  {
+    result[i] = str1[i];
+  }
+  size_t index = size1;
+  for (size_t i = 0; i < size2; ++i)
+  {
+    if (std::isdigit(static_cast<unsigned char>(str2[i])))
+    {
+      result[index++] = str2[i];
+    }
+  }
+  result[result_size] = '\0';
+  return result;
 }
 
 void ivantsova::printString(std::ostream & out, const char * str, size_t size)
