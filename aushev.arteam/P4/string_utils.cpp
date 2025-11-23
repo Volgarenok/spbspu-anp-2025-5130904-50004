4148d4152ffbf16e6a64a20f3c74e7bf098840a3
root <root@Asus.localdomain>
2025-11-23 18:12:04 +0300

    lab4: implement spc_rmv logic

--- aushev.arteam/P4/string_utils.cpp
+++ aushev.arteam/P4/string_utils.cpp
@@ -17,4 +17,26 @@ int has_sam(const char* s1, const char* s2) {
     return 0;
 }
 
+char* spc_rmv(char* dst, const char* src) {
+    if (!dst || !src) return nullptr;
+    char* out = dst;
+    const char* in = src;
+    while (*in == ' ') ++in;
+    bool space_pending = false;
+    while (*in) {
+        if (*in == ' ') {
+            space_pending = true;
+        } else {
+            if (space_pending) {
+                *out++ = ' ';
+                space_pending = false;
+            }
+            *out++ = *in;
+        }
+        ++in;
+    }
+    *out = '\0';
+    return dst;
+}
+
 }
