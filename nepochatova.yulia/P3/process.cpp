133a81febf0a48843010f78915887abaacd2134c
GenJune <julia.nepochatova@gmail.com>
2025-11-26 20:04:00 +0300

    transformMatrixSpiral наконец-то работает

--- nepochatova.yulia/P3/process.cpp
+++ nepochatova.yulia/P3/process.cpp
@@ -1,21 +1,37 @@
 #include "io.h"
 
-void Nepochatova::transformMatrixSpiral(int* arr, int n, int m)
-
-void Nepochatova::transformMatrixCircular(int* arr, int n, int m) {
-    int layers = (n < m ? n : m) / 2;
-
-    for (int layer = 0; layer < layers; ++layer) {
-        int increment = layer + 1;
-
-        for (int col = layer; col < m - layer; ++col) {
-            arr[layer * m + col] += increment;
-            arr[(n - layer - 1) * m + col] += increment;
+void Nepochatova::transformMatrixSpiral(int** arr, int n, int m) {
+    int top = 0;
+    int bottom = n - 1;
+    int left = 0;
+    int right = m-1;
+    int increment = 1;
+    while (top <= bottom && left <= right) {
+        for (int j = left; j <= right; j++) {
+            arr[bottom][j] += increment;
+            increment++;
         }
-
-        for (int row = layer + 1; row < n - layer - 1; ++row) {
-            arr[row * m + layer] += increment;
-            arr[row * m + (m - layer - 1)] += increment;
+        bottom--;
+        for (int i = bottom; i >= top; i--) {
+            arr[i][right] += increment;
+            increment++;
+        }
+        right--;
+        if (top <= bottom) {
+            for (int j = right; j >= left; j--) {
+                arr[top][j] += increment;
+                increment++;
+            }
+            top++;
+        }
+        if (left <= right) {
+            for (int i = top; i <= bottom; i++) {
+                arr[i][left] += increment;
+                increment++;
+            }
+            left++;
         }
     }
 }
+
+void Nepochatova::transformMatrixCircular(int** arr, int n, int m) {}
