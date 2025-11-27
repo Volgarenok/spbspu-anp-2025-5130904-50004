e70cc42b1c5a7dab138519a8640dfe24538d9876
maxchadin <maxchadin080418@gmail.com>
2025-11-28 01:33:57 +0300

    add digitSnd to strings.cpp

--- chadin.maxim/P4/strings.cpp
+++ chadin.maxim/P4/strings.cpp
@@ -4,13 +4,17 @@ namespace chadin
 {
 int hasSameSymbols(const char* str1, const char* str2)
 {
-  if (str1 == nullptr || str2 == nullptr) {
+  if (str1 == nullptr || str2 == nullptr)
+  {
     return 0;
   }
 
-  for (const char* p1 = str1; *p1 != '\0'; ++p1) {
-    for (const char* p2 = str2; *p2 != '\0'; ++p2) {
-      if (*p1 == *p2) {
+  for (const char* p1 = str1; *p1 != '\0'; ++p1)
+  {
+    for (const char* p2 = str2; *p2 != '\0'; ++p2)
+    {
+      if (*p1 == *p2)
+      {
         return 1;
       }
     }
@@ -20,6 +24,35 @@ int hasSameSymbols(const char* str1, const char* str2)
 
 int digitSnd(const char* str1, const char* str2, char* result, size_t bufferSize)
 {
+  if (str1 == nullptr || str2 == nullptr || result == nullptr || bufferSize == 0)
+  {
+    return 1;
+  }
+  size_t pos = 0;
+  for (size_t i = 0; str1[i] != '\0'; ++i)
+  {
+    if (pos >= bufferSize - 1)
+    {
+      return 1;
+    }
+    result[pos++] = str1[i];
+  }
+
+  for (size_t i = 0; str2[i] != '\0'; ++i)
+  {
+    char c = str2[i];
+    if (c >= '0' && c <= '9')
+    {
+      if (pos >= bufferSize - 1)
+      {
+        return 1;
+      }
+      result[pos++] = c;
+    }
+  }
+
+  result[pos] = '\0';
+  return 0;
 }
 }
 
