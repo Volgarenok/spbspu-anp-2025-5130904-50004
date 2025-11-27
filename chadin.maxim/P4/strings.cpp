64cc25f0418675cd80a57193df9a9b8c5d2c6512
maxchadin <maxchadin080418@gmail.com>
2025-11-28 01:25:20 +0300

    add strings.cpp

---
+++ chadin.maxim/P4/strings.cpp
@@ -0,0 +1,13 @@
+#include "strings.h"
+
+namespace chadin
+{
+int hasSameSymbols(const char* str1, const char* str2)
+{
+}
+
+int digitSnd(const char* str1, const char* str2, char* result, size_t bufferSize)
+{
+}
+}
+
