42022b281d6a490b206caff37e820393990a015d
zvxgich <dan011807@gmail.com>
2025-11-27 19:00:31 +0300

    I really hope there won't be any problems with the CG.

--- zvyagin.daniil/P3/functions.hpp
+++ zvyagin.daniil/P3/functions.hpp
@@ -1,14 +1,14 @@
 #ifndef ZVYAGIN_FUNCTIONS_HPP
 #define ZVYAGIN_FUNCTIONS_HPP
 
-#include <cstddef>
 #include <fstream>
+
 namespace zvyagin
 {
-    size_t count_columns_no_same_adjacent(const int* data, size_t rows, size_t cols);
-    size_t count_local_maxima(const int* data, size_t rows, size_t cols);
-    bool readMatrix(std::ifstream& input, int* data, size_t size);
-    void writeResults(std::ofstream& output, const int* data, size_t rows, size_t cols);
+  size_t count_columns_no_same_adjacent(const int* data, size_t rows, size_t cols);
+  size_t count_local_maxima(const int* data, size_t rows, size_t cols);
+  bool readMatrix(std::ifstream& input, int* data, size_t size);
+  void writeResults(std::ofstream& output, const int* data, size_t rows, size_t cols);
 }
 
 #endif
