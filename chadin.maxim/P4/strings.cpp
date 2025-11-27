a28c28dc5f922b2e0e6e4f4e7af9fe6a798420ce
maxchadin <maxchadin080418@gmail.com>
2025-11-28 01:29:14 +0300

    add hasSameSymbols to strings.cpp

--- chadin.maxim/P4/strings.cpp
+++ chadin.maxim/P4/strings.cpp
@@ -4,6 +4,18 @@ namespace chadin
 {
 int hasSameSymbols(const char* str1, const char* str2)
 {
+  if (str1 == nullptr || str2 == nullptr) {
+    return 0;
+  }
+
+  for (const char* p1 = str1; *p1 != '\0'; ++p1) {
+    for (const char* p2 = str2; *p2 != '\0'; ++p2) {
+      if (*p1 == *p2) {
+        return 1;
+      }
+    }
+  }
+  return 0;
 }
 
 int digitSnd(const char* str1, const char* str2, char* result, size_t bufferSize)
