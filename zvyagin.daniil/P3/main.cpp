663d180b8eb2dfcbf06d64292767f991e248bb96
zvxgich <dan011807@gmail.com>
2025-11-27 18:06:46 +0300

    CG 53 fix

--- zvyagin.daniil/P3/main.cpp
+++ zvyagin.daniil/P3/main.cpp
@@ -10,7 +10,6 @@ int main(int argc, char** argv)
         std::cerr << "Usage: ./lab num input output\n";
         return 1;
     }
-
     int num;
     try {
         num = std::stoi(argv[1]);
@@ -21,29 +20,24 @@ int main(int argc, char** argv)
         std::cerr << "First parameter is out of range\n";
         return 1;
     }
-
     if (num != 1 && num != 2)
     {
         std::cerr << "First parameter must be 1 or 2\n";
         return 1;
     }
-
     std::ifstream input(argv[2]);
     if (!input.is_open())
     {
         std::cerr << "Cannot open input file\n";
         return 2;
     }
-
     size_t n = 0, m = 0;
     if (!(input >> n >> m))
     {
         std::cerr << "Cannot read matrix dimensions\n";
         return 2;
     }
-
     std::ofstream output;
-
     if (n == 0 || m == 0)
     {
         output.open(argv[3]);
@@ -57,7 +51,6 @@ int main(int argc, char** argv)
         input.close();
         return 0;
     }
-
     if (num == 1)
     {
         int fixed[10000];
@@ -66,20 +59,16 @@ int main(int argc, char** argv)
             std::cerr << "Invalid matrix element data\n";
             return 2;
         }
-
         input.close();
-
         output.open(argv[3]);
         if (!output.is_open())
         {
             std::cerr << "Cannot open output file\n";
             return 1;
         }
-
         zvyagin::writeResults(output, fixed, n, m);
         output.close();
     }
-
     else
     {
         int* dyn = static_cast<int*>(std::malloc(n * m * sizeof(int)));
@@ -88,16 +77,13 @@ int main(int argc, char** argv)
             std::cerr << "Memory allocation failed\n";
             return 1;
         }
-
         if (!zvyagin::readMatrix(input, dyn, n * m))
         {
             std::free(dyn);
             std::cerr << "Invalid matrix element data\n";
             return 2;
         }
-
         input.close();
-
         output.open(argv[3]);
         if (!output.is_open())
         {
@@ -105,10 +91,8 @@ int main(int argc, char** argv)
             std::cerr << "Cannot open output file\n";
             return 1;
         }
-
         zvyagin::writeResults(output, dyn, n, m);
         output.close();
-
         std::free(dyn);
     }
     return 0;
