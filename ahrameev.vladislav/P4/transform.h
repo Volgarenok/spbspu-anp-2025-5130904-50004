12987e2be3f75bfbbf773e8acca12384a49707d4
EIKO7 <camper1773@mail.ru>
2025-11-28 21:03:10 +0300

    ver 1 trasnform

--- ahrameev.vladislav/P4/transform.h
+++ ahrameev.vladislav/P4/transform.h
@@ -1,6 +1,11 @@
 #ifndef TRANSFORM_H
 #define TRANSFORM_H
 
+namespace ahrameev {
 
+    int count_diff_latin_letters(const char* str);
+    int to_lower_latin(char* dest, size_t dest_size, const char* src);
+
+}
 
 #endif
