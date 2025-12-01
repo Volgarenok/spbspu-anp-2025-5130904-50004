948b24104f32ec2ce2f22857b850d40ccbda2e5f
Emich-M <fagetolrer@gmail.com>
2025-12-01 17:38:34 +0300

    Final setiing and translation units

--- em.maksim/P4/main.cpp
+++ em.maksim/P4/main.cpp
@@ -1,43 +1,7 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
-
-namespace em {
-
-void remove_latin(const char* src, char* dest) {
-    if (src == nullptr || dest == nullptr) return;
-    while (*src) {
-        char c = *src;
-        if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))) {
-            *dest = c;
-            dest++;
-        }
-        src++;
-    }
-    *dest = '\0';
-}
-
-int has_common_chars(const char* str1, const char* str2) {
-    if (str1 == nullptr || str2 == nullptr) return 0;
-
-    bool found[256] = {false};
-
-    while (*str1) {
-        found[static_cast<unsigned char>(*str1)] = true;
-        str1++;
-    }
-
-    while (*str2) {
-        if (found[static_cast<unsigned char>(*str2)]) {
-            return 1;
-        }
-        str2++;
-    }
-
-    return 0;
-}
-
-}
+#include "string.hpp"
 
 int main() {
     char input[10000];
