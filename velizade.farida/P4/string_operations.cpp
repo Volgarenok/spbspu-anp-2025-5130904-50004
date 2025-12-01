5a724eede6ed4683227ac81c978e83e8e9cc6c0e
Farida <velizadefarida@gmail.com>
2025-12-01 18:47:15 +0500

    added string reading function

---
+++ velizade.farida/P4/string_operations.cpp
@@ -0,0 +1,20 @@
+#include "velizade.hpp"
+#include <cstdlib>
+
+char* read_string(std::istream& input, size_t size)
+{
+  char* str = static_cast<char*>(malloc(size + 1));
+  if (!str)
+  {
+    return nullptr;
+  }
+  size_t i = 0;
+  inr ch = input.get();
+  while (i < size && ch != EOF && ch != '\n')
+  {
+    str[i++] = static_cast<char>(ch);
+    ch = input.get();
+  }
+  str[i] = '\0';
+  return str;
+}
