3eed48aeb046b756561a1c1cffff0a784d9623ad
Arda Boran Öztaş <oztas269@gmail.com>
2025-12-03 20:49:58 +0300

    Update tasks.hpp

--- oztas.arda/P3/tasks.hpp
+++ oztas.arda/P3/tasks.hpp
@@ -1,13 +1,15 @@
 #ifndef OZTAS_T3_TASKS_HPP
 #define OZTAS_T3_TASKS_HPP
 
+#include "matrix.hpp"
+
 namespace oztas
 {
-    // 18: CNT-NZR-DIG
-    int countNonZeroDiagonals(int* const* matrix, int rows, int cols);
+  // 18: CNT-NZR-DIG
+  int countNonZeroDiagonals(const int matrix[][MAX], int rows, int cols);
 
-    // 8: FLL-INC-WAV
-    void applyFillIncreasingWave(int** matrix, int rows, int cols);
+  // 8: FLL-INC-WAV
+  void applyFillIncreasingWave(int matrix[][MAX], int rows, int cols);
 }
 
 #endif
