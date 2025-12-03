18c6a7c9494e74365f41d7c805a8620c61f0ec35
Arda Boran Öztaş <oztas269@gmail.com>
2025-12-03 20:41:02 +0300

    Update matrix.hpp

--- oztas.arda/P3/matrix.hpp
+++ oztas.arda/P3/matrix.hpp
@@ -1,16 +1,17 @@
 #ifndef OZTAS_T3_MATRIX_HPP
 #define OZTAS_T3_MATRIX_HPP
-
+1
 #include <iosfwd>
 
 namespace oztas
 {
-  bool readMatrix(std::istream& input, int**& matrix, int& rows, int& cols);
+  const int MAX = 256;
 
-  void writeMatrix(std::ostream& output, int* const* matrix, int rows, int cols);
+  bool readMatrix(std::istream& input, int matrix[][MAX], int& rows, int& cols);
+
+  void writeMatrix(std::ostream& output, const int matrix[][MAX], int rows, int cols);
 
   void freeMatrix(int** matrix, int rows);
 }
 
 #endif
-
