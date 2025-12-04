970209308afdb6e75e3f0baf54eac7ea497069dc
BinaryKenny <khairullinvadim2006@gmail.com>
2025-12-04 18:19:48 +0300

    I delete extra conditions

--- khairullin.vadim/P3/main.cpp
+++ khairullin.vadim/P3/main.cpp
@@ -1,4 +1,4 @@
-#include "functions.hpp"
+#include "matrix.hpp"
 #include <iostream>
 #include <fstream>
 #include <algorithm>
@@ -19,12 +19,19 @@ int main(int argc, char ** argv)
   std::ofstream output(argv[3]);
   int rows = 0, cols = 0;
   input >> rows >> cols;
-  const size_t MIN_SIZE = std::min(rows, cols);
-  const size_t MAX_SIZE = std::max(rows, cols);
+  if (!rows && !cols)
+  {
+    output << 0 << "\n";
+    output << std::boolalpha;
+    output << true << "\n";
+    return 0;
+  }
   int * array = nullptr;
   int * square_array = nullptr;
-  int static_array[10000] = {};
-  int static_square_array[10000] = {};
+  int fixed_array[10000] = {};
+  int fixed_square_array[10000] = {};
+  const size_t MIN_SIZE = std::min(rows, cols);
+  const size_t MAX_SIZE = std::max(rows, cols);
   if (!input)
   {
     std::cerr << "File is empty\n";
@@ -35,69 +42,47 @@ int main(int argc, char ** argv)
     std::cerr << "Invalid rows or columns\n";
     return 2;
   }
-  else if (rows != 0 && cols != 0)
+  if (argv[1][0] == '1')
   {
-    if (argv[1][0] == '1')
+    array = fixed_array;
+    square_array = fixed_square_array;
+  }
+  else if (argv[1][0] == '2')
+  {
+    array = reinterpret_cast< int * > (std::malloc(rows * cols * sizeof(int)));
+    if (array == nullptr)
     {
-      array = static_array;
-      square_array = static_square_array;
+      std::cerr << "Bad alloc\n";
+      return 2;
     }
-    else if (argv[1][0] == '2')
+    square_array = reinterpret_cast< int * > (std::malloc(MIN_SIZE * MIN_SIZE * sizeof(int)));
+    if (square_array == nullptr)
     {
-      array = reinterpret_cast< int * > (std::malloc(rows * cols * sizeof(int)));
-      if (array == nullptr)
-      {
-        std::cerr << "Bad alloc\n";
-        return 2;
-      }
-      square_array = reinterpret_cast< int * > (std::malloc(MIN_SIZE * MIN_SIZE * sizeof(int)));
-      if (square_array == nullptr)
-      {
-        std::cerr << "Bad alloc\n";
-        free(array);
-        return 2;
-      }
-    }
-    try
-    {
-      khair::filling(array, input, rows, cols);
-    }
-    catch (const std::logic_error & err)
-    {
-      std::cout << err.what() << "\n";
-      if (argv[1][0] == '2')
-      {
-        free(array);
-        free(square_array);
-      }
-      return 1;
-    }
-    input.close();
-    output << khair::localMax(array, rows, cols) << "\n";
-    for (size_t i = 0; i < MIN_SIZE; i++)
-    {
-      for (size_t j = 0; j < MIN_SIZE; ++j)
-      {
-        square_array[MIN_SIZE * i + j] = array[MAX_SIZE * i + j];
-      }
-    }
-    if (khair::triangle(square_array, MIN_SIZE))
-    {
-      output << "True\n";
-    }
-    else
-    {
-      output << "False\n";
-    }
-    if (argv[1][0] == '2')
-    {
+      std::cerr << "Bad alloc\n";
       free(array);
-      free(square_array);
+      return 2;
     }
   }
-  else if (rows == 0 && cols == 0)
+  khair::fill(array, input, rows, cols);
+  if (input.fail())
   {
-    output << "Count of local maximums - 0\n";
-    output << "False\n";
+    std::cout << "Not enough elements\n";
+    return 2;
+  }
+  input.close();
+  output << khair::findLocalMax(array, rows, cols) << "\n";
+  for (size_t i = 0; i < MIN_SIZE; i++)
+  {
+    for (size_t j = 0; j < MIN_SIZE; ++j)
+    {
+      square_array[MIN_SIZE * i + j] = array[MAX_SIZE * i + j];
+    }
+  }
+  output << std::boolalpha;
+  output << khair::checkTriangle(square_array, MIN_SIZE) << "\n";
+  if (argv[1][0] == '2')
+  {
+    free(array);
+    free(square_array);
   }
 }
