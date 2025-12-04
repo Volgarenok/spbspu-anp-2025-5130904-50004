b66437bccb245257d0e78294547facc090e37b01
isaacgreenfield <144553175+isaacgreenfield@users.noreply.github.com>
2025-12-04 11:57:19 +0300

    Test-apt n-revision, disc.

--- ivanov.timur/P4/string_ideas.h
+++ ivanov.timur/P4/string_ideas.h
@@ -5,15 +5,11 @@
 namespace ivanov {
   char * spc_rmv(char *content, size_t &size);
   char * del_lat(char *content, size_t &size);
-
   void output(const char *content);
   char * merge(char *content1, const char *content2, size_t &size1, size_t size2);
-
   char * get_find(char *content, size_t &size);
-
   bool find(char symbol, const char *content);
   size_t count(char *content, char symbol);
-
   char * get_line(std::istream &in, char *data, size_t &size, size_t &length);
 }
 #endif
