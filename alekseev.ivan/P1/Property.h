90c84ef0687be0441edc5d8039e8fa8b7f6cfe1b
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-12-10 14:47:22 +0300

    Base Property.cpp

--- alekseev.ivan/P1/Property.h
+++ alekseev.ivan/P1/Property.h
@@ -2,19 +2,21 @@
 #define PROPERTY_H
 #include <cstddef>
 
-struct Property {
-  void operator()(int a);
-  size_t operator()() const;
-  char * name();
+namespace alekseev {
+  struct Property {
+    void operator()(int a);
+    size_t operator()() const;
+    char * name();
 
-  virtual ~Property();
+    virtual ~Property() = default;
 
-  private:
-    size_t k;
-    int * data;
+    private:
+      size_t k;
+      int * data;
 
-    virtual void update(int a);
-    virtual void value();
-    virtual char * my_name();
-};
+      virtual void update(int a) = 0;
+      virtual size_t value() const = 0;
+      virtual char * my_name() = 0;
+  };
+}
 #endif
