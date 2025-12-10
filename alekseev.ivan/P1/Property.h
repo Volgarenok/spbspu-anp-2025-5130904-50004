0f8b21168934dc47958ad4a1ae63a2e3eaa2c94f
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-12-10 15:18:40 +0300

    Pth_trp v1.0 update main: add make

--- alekseev.ivan/P1/Property.h
+++ alekseev.ivan/P1/Property.h
@@ -11,9 +11,6 @@ namespace alekseev {
     virtual ~Property() = default;
 
     private:
-      size_t k;
-      int * data;
-
       virtual void update(int a) = 0;
       virtual size_t value() const = 0;
       virtual char * my_name() = 0;
--- alekseev.ivan/P1/main.cpp
+++ alekseev.ivan/P1/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "Property.h"
+#include "Pth_trp.h"
 
+
+namespace alekseev {
+  Property * make(int n);
+}
 
 int main()
 {
   constexpr size_t NUM_OF_PROPS = 2;
-  Property * properties[NUM_OF_PROPS]{&Pth_trp, &Sum_dup};
+  alekseev::Property * properties[NUM_OF_PROPS]{alekseev::make(0), alekseev::make(1)};
   int a = 0;
   std::cin >> a;
   while (a && std::cin) {
@@ -16,4 +22,16 @@ int main()
   for (size_t i = 0; i < NUM_OF_PROPS; i++) {
     std::cout << properties[i].name() << ": " << properties[i]() << "\n";
   }
+}
+
+
+alekseev::Property * alekseev::make(int n)
+{
+  if (n == 0) {
+    return new Pth_trp();
+  }
+  if (n == 1) {
+    return new Sum_dup();
+  }
+  return nullptr;
 }
