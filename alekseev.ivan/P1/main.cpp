b2106a3b7599e62bb992db8d929acda3e9b9c7fe
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2025-12-10 14:35:04 +0300

    Base main

--- alekseev.ivan/P1/main.cpp
+++ alekseev.ivan/P1/main.cpp
@@ -1,1 +1,19 @@
-int main() {}
+#include <iostream>
+
+
+int main()
+{
+  constexpr size_t NUM_OF_PROPS = 2;
+  Property * properties[NUM_OF_PROPS]{&Pth_trp, &Sum_dup};
+  int a = 0;
+  std::cin >> a;
+  while (a && std::cin) {
+    for (size_t i = 0; i < NUM_OF_PROPS; i++) {
+      properties[i](a);
+    }
+    std::cin >> a;
+  }
+  for (size_t i = 0; i < NUM_OF_PROPS; i++) {
+    std::cout << properties[i].name() << ": " << properties[i]() << "\n";
+  }
+}
