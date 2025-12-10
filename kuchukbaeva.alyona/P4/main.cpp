6dc4ce179ce0b4ea54d261dd4e5ecd6fd584ad18
SoundwaveAI <ghost10.town10000@gmail.com>
2025-12-10 17:45:27 +0300

    ispravlena utechka pamiati

--- kuchukbaeva.alyona/P4/main.cpp
+++ kuchukbaeva.alyona/P4/main.cpp
@@ -4,28 +4,31 @@
 
 int main()
 {
+  size_t len = 0;
+  char* input_str = kuchukbaeva::readStr(std::cin, len);
+  if (input_str == nullptr)
+  {
+    std::cerr << "Empty input" << "\n";
+    return 1;
+  }
+  char* res_str = nullptr;
   try
   {
-    size_t len = 0;
-    char* input_str = kuchukbaeva::readStr(std::cin, len);
-    if (input_str == nullptr)
-    {
-      std::cerr << "Empty input" << "\n";
-      return 1;
-    }
-    char* res_str = new char[len + 1];
-    kuchukbaeva::rmvVow(input_str, res_str);
-    std::cout << res_str << "\n";
-    int repeatdig = kuchukbaeva::repDgt(input_str);
-    std::cout << repeatdig << "\n";
-    delete[] input_str;
-    delete[] res_str;
-    return 0;
+    res_str = new char[len + 1];
   }
   catch (const std::bad_alloc& e)
   {
-    std::cerr << "Memory allocation failed (" << e.what() << ")" << "\n";
+    delete[] input_str;
+    std::cerr << "Troubles with memory: " << e.what() << "\n";
     return 1;
   }
+  kuchukbaeva::rmvVow(input_str, res_str);
+  std::cout << res_str << "\n";
+  int repeatdig = kuchukbaeva::repDgt(input_str);
+  std::cout << repeatdig << "\n";
+  delete[] input_str;
+  delete[] res_str;
+  return 0;
 }
+
 
