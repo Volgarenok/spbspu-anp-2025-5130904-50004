6b3c1fd89ae24a37c65629dcf4a20c627d06487f
SoundwaveAI <ghost10.town10000@gmail.com>
2025-12-12 18:44:25 +0300

    realized logic in cpp for eqlseq

---
+++ kuchukbaeva.alyona/P1/EqlSeq.cpp
@@ -0,0 +1,31 @@
+#include "SeqEql.hpp"
+
+namespace kuchukbaeva
+{
+  EqlSeq::EqlSeq() :
+    max_(0),
+    cur_(0),
+    last_(0),
+    cnt_(0)
+  {}
+
+  void EqlSeq::operator()(int val)
+  {
+    if (cnt_ == 0 || val != last_) {
+      cur_ = 1;
+      last_ = val;
+    } else {
+      cur_++;
+    }
+    if (cur_ > max_) {
+      max_ = cur_;
+    }
+    cnt_++;
+  }
+
+  size_t EqlSeq::get() const
+  {
+    return max_;
+  }
+}
+
