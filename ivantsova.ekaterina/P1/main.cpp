1039389ce6ba74aae9d08577640dd4cf76e16dda
ekaterinaivanc <kataivancova7@gmail.com>
2025-12-12 21:23:34 +0300

    Add CountDivisible

--- ivantsova.ekaterina/P1/main.cpp
+++ ivantsova.ekaterina/P1/main.cpp
@@ -47,6 +47,36 @@ namespace ivantsova
 
   struct CountDivisible
   {
+    CountDivisible():
+    prev_(0),
+    count_(0),
+    hasPrev_(false)
+    {}
+
+    void operator()(int a)
+    {
+      if (hasPrev_ && prev_ != 0 && a % prev_ == 0)
+      {
+        ++count_;
+      }
+      prev_ = a;
+      hasPrev_ = true;
+    }
+
+    size_t operator()()const
+    {
+      return count_;
+    }
+
+    bool isValid()const
+    {
+      return hasPrev_;
+    }
+
+  private:
+    int prev_;
+    size_t count_;
+    bool hasPrev_;
   };
 }
 
