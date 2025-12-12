e8e91fb8d1989cf3a870ef36bb1f6ba4d936986e
ekaterinaivanc <kataivancova7@gmail.com>
2025-12-12 20:19:05 +0300

    Add CountMax

--- ivantsova.ekaterina/P1/main.cpp
+++ ivantsova.ekaterina/P1/main.cpp
@@ -4,6 +4,45 @@ namespace ivantsova
 {
   struct CountMax
   {
+    CountMax():
+    maxInit_(false),
+    max_(0),
+    count_(0)
+    {}
+
+    void operator()(int a)
+    {
+      if (!maxInit_)
+      {
+        max_ = a;
+        count_ = 1;
+        maxInit_ = true;
+      }
+      else if (a > max_)
+      {
+        max_ = a;
+        count_ = 1;
+      }
+      else if (a == max_)
+      {
+        ++count_;
+      }
+    }
+
+    size_t operator()()const
+    {
+      return count_;
+    }
+
+    bool isValid()const
+    {
+      return maxInit_;
+    }
+
+  private:
+    bool maxInit_;
+    int max_;
+    size_t count_;
   };
 
   struct CountDivisible
