1b86fb9efd3661ed84f5fa897fd4653534dd8663
SoundwaveAI <ghost10.town10000@gmail.com>
2025-12-12 18:41:18 +0300

    realized logic of submax in cpp

---
+++ kuchukbaeva.alyona/P1/SubMax.cpp
@@ -0,0 +1,31 @@
+#include "SubMax.hpp"
+#include <limits>
+#include <stdexcept>
+
+namespace kuchukbaeva {
+  SubMax::SubMax() :
+    max_(std::numeric_limits<int>::min()),
+    sec_(std::numeric_limits<int>::min()),
+    cnt_(0)
+  {}
+
+  void SubMax::operator()(int v) {
+    if (cnt_ == 0) {
+      max_ = v;
+    } else if (v > max_) {
+      sec_ = max_;
+      max_ = v;
+    } else if (v > sec_) {
+      sec_ = v;
+    }
+    ++cnt_;
+  }
+
+  int SubMax::res() const {
+    return sec_;
+  }
+
+  bool SubMax::has() const {
+    return cnt_ >= 2;
+  }
+}
