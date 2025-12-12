de5129e574496f2559cce621bf1a80640ce0b0be
SoundwaveAI <ghost10.town10000@gmail.com>
2025-12-12 18:46:27 +0300

    realized main with functions and return codi vozvrata

--- kuchukbaeva.alyona/P1/main.cpp
+++ kuchukbaeva.alyona/P1/main.cpp
@@ -1,3 +1,34 @@
+#include "SubMax.hpp"
+#include "EqlSeq.hpp"
+#include <iostream>
+
 int main()
-{}
+{
+  kuchukbaeva::SubMax sub_max;
+  kuchukbaeva::EqlSeq eql_seq;
+
+  int val = 0;
+  while (std::cin >> val && val != 0) {
+    sub_max(val);
+    eql_seq(val);
+  }
+
+  if (std::cin.fail() && !std::cin.eof()) {
+    std::cerr << "Input error" << "\n";
+    return 1;
+  }
+
+  int ret = 0;
+
+  if (sub_max.has()) {
+    std::cout << sub_max.res() << "\n";
+  } else {
+    std::cerr << "Error: not enough elements for SUB-MAX" << "\n";
+    ret = 2;
+  }
+
+  std::cout << eql_seq.res() << "\n";
+  return ret;
+}
+
 
