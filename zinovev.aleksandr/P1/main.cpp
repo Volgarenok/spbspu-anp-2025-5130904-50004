8ea0d0cadcaed14841fe05f424031e4f5b07b3b4
FShaaM <zinovevas135@gmail.com>
2025-12-14 22:18:58 +0300

    Added Base-Class

--- zinovev.aleksandr/P1/main.cpp
+++ zinovev.aleksandr/P1/main.cpp
@@ -1,99 +1,109 @@
 #include <iostream>
 
-namespace zinovev
-{
+namespace zinovev {
 
-class RTH_TRP
+struct Base
 {
-public:
-  RTH_TRP() :
+  virtual void operator()() const = 0;
+  virtual void operator()(int const) = 0;
+  virtual ~Base() = default;
+};
+
+struct RTH_TRP : Base
+{
+  RTH_TRP():
+    Base(),
+    pth_trp(0),
     a(0),
     b(0),
-    c(0),
-    pth_trp(0)
+    c(0)
   {}
 
-  void process(int x)
-  {
-    a = b;
-    b = c;
-    c = x;
-
-    if (a != 0 && b != 0 && c != 0)
-    {
-      if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
-      {
-        ++pth_trp;
-      }
-    }
-  }
-
-  void print_result() const
-  {
-    if (few_numbers())
-    {
-      std::cerr << "ERROR: few numbers for RTH_TRP\n";
-    }
-    else
-    {
-      std::cout << "RTH_TRP: " << pth_trp << "\n";
-    }
-  }
-
-  bool few_numbers() const
-  {
-    return a == 0;
-  }
+  void operator()() const override;
+  void operator()(int const x) override;
+  bool few_numbers();
 
 private:
-  int a, b, c;
+  int a = 0, b = 0, c = 0;
   size_t pth_trp;
 };
 
-class EQL_SEQ
+struct EQL_SEQ : Base
 {
-public:
   EQL_SEQ() :
+    Base(),
     max(0),
     eql_seq(0)
   {}
 
-  void process(int x)
-  {
-    if (max < x)
-    {
-      max = x;
-      eql_seq = 1;
-    }
-    else if (max == x)
-    {
-      ++eql_seq;
-    }
-  }
-
-  void print_result() const
-  {
-    std::cout << "EQL_SEQ: " << eql_seq << "\n";
-  }
+  void operator()() const override;
+  void operator()(int const x) override;
 
 private:
   int max;
   size_t eql_seq;
 };
 
+void RTH_TRP::operator()() const
+{
+  if (a == 0)
+  {
+    std::cerr << "ERROR: few numbers for RTH_TRP\n";
+  }
+  else
+  {
+    std::cout << "RTH_TRP: " << pth_trp << "\n";
+  }
 }
+
+void RTH_TRP::operator()(int const x)
+{
+  a = b;
+  b = c;
+  c = x;
+
+  if (a * a + b * b == c * c || a * a + c * c == b * b || b * b + c * c == a * a)
+  {
+    ++pth_trp;
+  }
+}
+
+bool RTH_TRP::few_numbers()
+{
+  return a == 0 ? true : false;
+}
+
+void EQL_SEQ::operator()() const
+{
+  std::cout << "EQL_SEQ: " << eql_seq << "\n";
+}
+
+void EQL_SEQ::operator()(int const x)
+{
+  if (max < x)
+  {
+    max = x;
+    eql_seq = 1;
+  }
+  else if (max == x)
+  {
+    ++eql_seq;
+  }
+}
+
+} // namespace zinovev
 
 int main()
 {
-  zinovev::RTH_TRP r;
   zinovev::EQL_SEQ e;
+  zinovev::RTH_TRP r;
 
   int x = 0;
 
   while (std::cin >> x && x != 0)
   {
-    r.process(x);
-    e.process(x);
+    r(x);
+    e(x);
   }
 
   if (std::cin.fail())
@@ -102,13 +112,11 @@ int main()
     return 1;
   }
 
-  r.print_result();
-  e.print_result();
+  r();
+  e();
 
   if (r.few_numbers())
   {
     return 2;
   }
-
-  return 0;
 }
