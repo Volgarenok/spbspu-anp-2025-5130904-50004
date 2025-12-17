444d2ff4c52b84e481bf6382a5f0c369d7ed3af7
BinaryKenny <khairullinvadim2006@gmail.com>
2025-12-17 16:08:30 +0300

    I deleted an extra ws

--- khairullin.vadim/P1/main.cpp
+++ khairullin.vadim/P1/main.cpp
@@ -4,23 +4,23 @@
 #include "Decrease.hpp"
 namespace khair = khairullin;
 int main()
-{  
+{
   int number =  0;
   khair::Maximums M;
   khair::Decrease D;
   while (std::cin >> number and number != 0)
+  {
+    if (number == 0)
     {
-        if (number == 0)
-        {
-            break;
-        }
-        M(number);
-        D(number);
+      break;
     }
-    if (std::cin.fail())
-    {
-        std::cerr << "Something goes wrong\n";
-        return 1;
-    }
-    std::cout << M() << " " << D() << "\n";
+    M(number);
+    D(number);
+  }
+  if (std::cin.fail())
+  {
+    std::cerr << "Something goes wrong\n";
+    return 1;
+  }
+  std::cout << M() << " " << D() << "\n";
 }
