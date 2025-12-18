91af81c29d9443b8f47dad4ed9cfec5cb4e5f76c
emiraydgn <emiraydogan03@gmail.com>
2025-12-18 12:23:03 +0300

    fixed void to char

--- aydogan.emir/P4/string.hpp
+++ aydogan.emir/P4/string.hpp
@@ -6,8 +6,8 @@
 namespace aydogan
 {
   char * readString(std::istream & in);
-  void removeVowels(const char * src, char * dest) noexcept;
-  void appendDigits(const char * first, const char * second, char * dest) noexcept;
+  char * removeVowels(const char * src, char * dest) noexcept;
+  char * appendDigits(const char * first, const char * second, char * dest) noexcept;
   void writeString(const char * str);
 }
 
