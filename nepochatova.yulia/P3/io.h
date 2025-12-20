a31fa8370b5ce660a127d12b18d47528aefcdd4c
GenJune <julia.nepochatova@gmail.com>
2025-12-20 22:22:22 +0300

    Fixed program 1

--- nepochatova.yulia/P3/io.h
+++ nepochatova.yulia/P3/io.h
@@ -5,7 +5,7 @@
 namespace nepochatova
 {
   int checkArgs(int argc, char ** argv);
-  int ** readMatrix(const std::string& filename, size_t& n, size_t& m);
+  int ** readMatrix(const std::string& filename, size_t& n, size_t& m, bool useDynamic);
   void writeMatrix(std::ofstream& out, const int* const* arr, size_t n, size_t m);
   void transformMatrixSpiral (int** arr, size_t n, size_t m);
   void transformMatrixCircular(int** arr, size_t n, size_t m);
