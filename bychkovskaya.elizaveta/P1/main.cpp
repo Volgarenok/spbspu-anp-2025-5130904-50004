a074e47ae0d986369517e6fba9c8e48892f83a73
laloctepp <lbychkovskaya.omsk@gmail.com>
2025-12-26 20:55:00 +0300

    simple implementation of CNT-MAX

--- bychkovskaya.elizaveta/P1/main.cpp
+++ bychkovskaya.elizaveta/P1/main.cpp
@@ -1,20 +1,50 @@
 #include <iostream>
 
+// int main() {
+//   int num = 0;
+//   int prevNum = 0;
+//   size_t count = 0;
+//   size_t k = 0;
+// 	while (std::cin >> num && num != 0) {
+// 		++k;
+// 		if (num > prevNum && k != 1) {
+// 			++count;
+// 		}
+// 		prevNum = num;
+// 	}
+// 	if (!std::cin && !std::cin.eof()) {
+// 		std::cerr << "Wrong input" << "\n";
+// 		return 1;
+// 	}
+// 	std::cout << count << "\n";
+// }
+
 int main() {
-    int num = 0;
-    int prevNum = 0;
-    size_t count = 0;
-   size_t k = 0;
-    while (std::cin >> num && num != 0) {
-        ++k;
-        if (num > prevNum && k != 1) {
-            ++count;
-        }
-        prevNum = num;
+  int num = 0;
+  int max = 0;
+  size_t count = 0;
+  size_t k = 0;
+  while (std::cin >> num && num != 0) {
+    ++k;
+		if (k == 1) {
+			max = num;
+			count = 1;
+		}
+    if (num > max && k != 1) {
+      count = 1;
     }
-    if (!std::cin && !std::cin.eof()) {
-        std::cerr << "Wrong input" << "\n";
-        return 1;
-    }
-    std::cout << count << "\n";
+		if (num == max && k != 1) {
+			++count;
+		}
+  }
+	if (num == 0 && k == 0) {
+		std::cerr << "Not enough elements" << "\n";
+		return 2;
+	}
+  if (!std::cin) {
+    std::cerr << "Wrong input" << "\n";
+    return 1;
+  }
+
+  std::cout << count << "\n";
 }
