cccbb492faf464c5c9126a1d146a2c1a6b5b4f82
EIKO7 <camper1773@mail.ru>
2025-12-28 22:49:14 +0300

    ver 5

--- ahrameev.vladislav/P1/main.cpp
+++ ahrameev.vladislav/P1/main.cpp
@@ -5,13 +5,19 @@ int main()
 {
     auto res = ahrammev::compute();
 
-    if (std::cin.fail())
-    {
+    if (std::cin.fail()) {
         std::cerr << "Error\n";
         return 1;
     }
 
     std::cout << res.cntMin << '\n';
-    std::cout << res.aftMax << '\n';
-    return 0;
+
+    if (res.aftMaxValid) {
+        std::cout << res.aftMax << '\n';
+        return 0;
+    }
+    else {
+        std::cerr << "Error\n";
+        return 2;
+    }
 }
