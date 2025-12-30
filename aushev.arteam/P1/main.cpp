d745a3f88785685b3e180389b3283d489ffe9405
Ausev.Arteam <seregabest77@gmail.com>
2025-12-31 00:50:40 +0300

    commit 4

--- aushev.arteam/P1/main.cpp
+++ aushev.arteam/P1/main.cpp
@@ -53,8 +53,19 @@ bool process_sequence(int& eql_result, int& mon_result) {
   return true;
 }
 
-} 
+} // namespace aushev
 
 int main() {
+  int eql_out;
+  int mon_out;
+
+  if (!aushev::process_sequence(eql_out, mon_out)) {
+    std::cerr << "Invalid input\n";
+    return 1;
+  }
+
+  std::cout << eql_out << '\n';
+  std::cout << mon_out << '\n';
+
   return 0;
 }
