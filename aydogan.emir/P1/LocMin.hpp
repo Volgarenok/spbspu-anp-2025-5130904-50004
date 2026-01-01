770ec92c3e4fb16284a66cbf6b114fda2d0e8539
emiraydgn <emiraydogan03@gmail.com>
2026-01-01 23:14:44 +0300

    Added struct to LocMin.hpp

--- aydogan.emir/P1/LocMin.hpp
+++ aydogan.emir/P1/LocMin.hpp
@@ -5,7 +5,19 @@
 
 namespace aydogan
 {
-  
+  struct LocMin
+  {
+    LocMin();
+    void operator()(int n);
+    size_t operator()() const;
+
+   private:
+    size_t k_;
+    int a_;
+    int b_;
+    bool has_a_;
+    bool has_b_;
+  };
 }
 
 #endif
