4f8253c5eaa1c92998b9bff175138f0aaa27f47c
emiraydgn <emiraydogan03@gmail.com>
2026-01-01 23:29:45 +0300

    Added main.cpp

--- aydogan.emir/P1/main.cpp
+++ aydogan.emir/P1/main.cpp
@@ -1,3 +1,26 @@
-int main() {
-    return 0;
+#include <iostream>
+#include "MonInc.hpp"
+#include "LocMin.hpp"
+
+int main()
+{
+  int num = 0;
+  aydogan::MonInc mon_inc;
+  aydogan::LocMin loc_min;
+
+  while (std::cin >> num && num != 0)
+  {
+    mon_inc(num);
+    loc_min(num);
+  }
+
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "Input error\n";
+    return 1;
+  }
+
+  std::cout << mon_inc() << "\n";
+  std::cout << loc_min() << "\n";
+  return 0;
 }
