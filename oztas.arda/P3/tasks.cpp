1407ecb74a0fb5fc620262916bd419d78586ff02
Arda Boran Öztaş <oztas269@gmail.com>
2026-01-05 22:00:22 +0300

    Update tasks.cpp

--- oztas.arda/P3/tasks.cpp
+++ oztas.arda/P3/tasks.cpp
@@ -1,69 +1,70 @@
 #include "tasks.hpp"
 
-namespace oztas
+int oztas::countNonZeroDiagonals(const int* matrix, int rows, int cols)
 {
-  int countNonZeroDiagonals(const int* matrix, int rows, int cols)
-  {
-    int result = 0;
+  int result = 0;
 
-    const int minOffset = -rows + 1;
-    const int maxOffset = cols - 1;
+  const int minOffset = -rows + 1;
+  const int maxOffset = cols - 1;
 
-    for (int offset = minOffset; offset <= maxOffset; ++offset) {
-      bool hasElement = false;
-      bool allNonZero = true;
+  for (int offset = minOffset; offset <= maxOffset; ++offset) {
+    bool hasElement = false;
+    bool allNonZero = true;
 
-      for (int i = 0; i < rows; ++i) {
-        const int j = i + offset;
-        if (j < 0 || j >= cols) {
-          continue;
-        }
-
-        hasElement = true;
-        if (matrix[i * cols + j] == 0) {
-          allNonZero = false;
-          break;
-        }
+    for (int i = 0; i < rows; ++i) {
+      const int j = i + offset;
+      if (j < 0 || j >= cols) {
+        continue;
       }
 
-      if (hasElement && allNonZero) {
-        ++result;
+      hasElement = true;
+      if (matrix[i * cols + j] == 0) {
+        allNonZero = false;
+        break;
       }
     }
 
-    return result;
+    if (hasElement && allNonZero) {
+      ++result;
+    }
   }
 
-  void applyFillIncreasingWave(int* matrix, int rows, int cols)
-  {
-    int value = 1;
-    int top = 0, bottom = rows - 1;
-    int left = 0, right = cols - 1;
+  return result;
+}
 
-    while (top <= bottom && left <= right) {
-      for (int j = left; j < right; ++j) {
-        matrix[top * cols + j] = value++;
-      }
-      top++;
+void oztas::applyFillIncreasingWave(int* matrix, int rows, int cols)
+{
+  int value = 1;
 
-      for (int i = top; i <= bottom; ++i) {
-        matrix[i * cols + right] = value++;
-      }
-      right--;
+  int top = 0;
+  int bottom = rows - 1;
 
-      if (top <= bottom) {
-        for (int j = right; j >= left; --j) {
-          matrix[bottom * cols + j] = value++;
-        }
-        bottom--;
-      }
+  int left = 0;
+  int right = cols - 1;
 
-      if (left <= right) {
-        for (int i = bottom; i >= top; --i) {
-          matrix[i * cols + left] = value++;
-        }
-        left++;
+  while (top <= bottom && left <= right) {
+    for (int j = left; j < right; ++j) {
+      matrix[top * cols + j] = value++;
+    }
+    top++;
+
+    for (int i = top; i <= bottom; ++i) {
+      matrix[i * cols + right] = value++;
+    }
+    right--;
+
+    if (top <= bottom) {
+      for (int j = right; j >= left; --j) {
+        matrix[bottom * cols + j] = value++;
       }
+      bottom--;
+    }
+
+    if (left <= right) {
+      for (int i = bottom; i >= top; --i) {
+        matrix[i * cols + left] = value++;
+      }
+      left++;
     }
   }
 }
