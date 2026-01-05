57fd6e10f2c43369daaae190f2a1a0a2b1cd7e88
Arda Boran Öztaş <oztas269@gmail.com>
2026-01-05 22:49:57 +0300

    Update main.cpp

--- oztas.arda/P3/main.cpp
+++ oztas.arda/P3/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <fstream>
-
+#include <cstdlib>
 #include "matrix.hpp"
 #include "tasks.hpp"
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
   if (argc != 4) {
     std::cerr << "invalid arguments\n";
     return 1;
@@ -39,20 +38,15 @@ int main(int argc, char* argv[])
     return 2;
   }
 
-  int rows = 0;
-  int cols = 0;
+  size_t rows = 0;
+  size_t cols = 0;
 
   if (!(input >> rows >> cols)) {
     std::cerr << "invalid matrix data\n";
     return 2;
   }
 
-  if (rows < 0 || cols < 0) {
-    std::cerr << "invalid matrix data\n";
-    return 2;
-  }
-
-  constexpr int MAX = 256;
+  constexpr size_t MAX = 256;
   if (rows > MAX || cols > MAX) {
     std::cerr << "invalid matrix data\n";
     return 2;
