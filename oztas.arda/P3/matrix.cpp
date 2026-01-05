da6f87f3c796bce5e9d61017fa89379e39179801
Arda Boran Öztaş <oztas269@gmail.com>
2026-01-05 22:29:35 +0300

    Update matrix.cpp

--- oztas.arda/P3/matrix.cpp
+++ oztas.arda/P3/matrix.cpp
@@ -2,53 +2,46 @@
 #include <istream>
 #include <ostream>
 
-namespace oztas
+bool oztas::readMatrix(std::istream& input,
+                       int matrix[],
+                       int rows,
+                       int cols)
 {
-  bool readMatrix(std::istream& input,
-                  int matrix[],
-                  int& rows,
-                  int& cols)
-  {
-    if (!(input >> rows >> cols)) {
-      return false;
-    }
+  if (rows < 0 || cols < 0) {
+    return false;
+  }
 
-    if (rows < 0 || cols < 0) {
-      return false;
-    }
-
-    if (rows == 0 || cols == 0) {
-      return true;
-    }
-
-    const size_t srows = static_cast< size_t >(rows);
-    const size_t scols = static_cast< size_t >(cols);
-
-    for (size_t i = 0; i < srows; ++i) {
-      for (size_t j = 0; j < scols; ++j) {
-        if (!(input >> matrix[i * scols + j])) {
-          return false;
-        }
-      }
-    }
-
+  if (rows == 0 || cols == 0) {
     return true;
   }
 
-  void writeMatrix(std::ostream& output,
-                   const int matrix[],
-                   int rows,
-                   int cols)
-  {
-    output << rows << " " << cols;
+  const size_t srows = static_cast< size_t >(rows);
+  const size_t scols = static_cast< size_t >(cols);
 
-    const size_t srows = static_cast< size_t >(rows);
-    const size_t scols = static_cast< size_t >(cols);
-
-    for (size_t i = 0; i < srows; ++i) {
-      for (size_t j = 0; j < scols; ++j) {
-        output << " " << matrix[i * scols + j];
+  for (size_t i = 0; i < srows; ++i) {
+    for (size_t j = 0; j < scols; ++j) {
+      if (!(input >> matrix[i * scols + j])) {
+        return false;
       }
+    }
+  }
+
+  return true;
+}
+
+void oztas::writeMatrix(std::ostream& output,
+                        const int matrix[],
+                        int rows,
+                        int cols)
+{
+  output << rows << " " << cols;
+
+  const size_t srows = static_cast< size_t >(rows);
+  const size_t scols = static_cast< size_t >(cols);
+
+  for (size_t i = 0; i < srows; ++i) {
+    for (size_t j = 0; j < scols; ++j) {
+      output << " " << matrix[i * scols + j];
     }
   }
 }
