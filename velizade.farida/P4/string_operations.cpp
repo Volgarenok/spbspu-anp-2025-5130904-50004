8bc2e0f47eb33cb1808df44a54a78ae7667570a6
Farida <velizadefarida@gmail.com>
2026-01-11 04:43:13 +0500

    fixed main and string operations

--- velizade.farida/P4/string_operations.cpp
+++ velizade.farida/P4/string_operations.cpp
@@ -1,139 +1,79 @@
 #include "string_operations.hpp"
 #include <cstdlib>
+#include <ios>
 
-char* velizade::read_string(std::istream& input, size_t& length)
+char* velizade::read_string(std::istream& input, size_t size)
 {
-  const size_t initial_size = 16;
-  size_t capacity = initial_size;
-  char* str = reinterpret_cast<char*>(malloc(capacity));
+  char* str = static_cast<char*>(malloc(size + 1));
   if (!str)
   {
-    length = 0;
     return nullptr;
   }
-  length = 0;
+  std::ios::fmtflags original_flags = input.flags();
+  input >> std::noskipws;
+  size_t i = 0;
   char ch;
-  while (input >> std::noskipws >> ch && ch != '\n')
+ try
   {
-    if (length >= capacity - 1)
+    while (i < size && input >> ch && ch != '\n')
     {
-      capacity *= 2;
-      char* new_str = reinterpret_cast<char*>(realloc(str, capacity));
-      if (!new_str)
+      str[i++] = ch;
+    }
+    str[i] = '\0';
+    if (i == size && input && ch != '\n')
+    {
+      while (input >> ch && ch != '\n')
       {
-        free(str);
-        length = 0;
-        return nullptr;
       }
-      str = new_str;
     }
-    str[length] = ch;
-    length++;
   }
-  str[length] = '\0';
-  if (length < capacity - 1)
+  catch (const std::ios::failure& e)
   {
-    char* optimized_str = reinterpret_cast<char*>(realloc(str, length + 1));
-    if (optimized_str)
-    {
-      str = optimized_str;
-    }
+    input.flags(original_flags);
+    free(str);
+    throw;
   }
+  input.flags(original_flags);
   return str;
 }
 
-char* velizade::rep_sym(const char* str)
+int velizade::rep_sym(char* buffer, const char* str)
 {
-  if (!str)
+  if (!buffer || !str)
   {
-    return nullptr;
-  }
-  size_t unique_count = 0;
-  for (size_t i = 0; str[i]; i++)
-  {
-    int count = 0;
-    for(size_t j = 0; str[j]; j++)
-    {
-      if (str[i] == str[j])
-      {
-        count++;
-      }
-    }
-    if (count > 1)
-    {
-      bool already_counted = false;
-      for (size_t k = 0; k < i; k++)
-      {
-        if (str[k] == str[i])
-        {
-          already_counted = true;
-          break;
-        }
-      }
-      if (!already_counted)
-      {
-        unique_count++;
-      }
-    }
-  }
-  char* buffer = reinterpret_cast<char*>(malloc(unique_count + 1));
-  if (!buffer)
-  {
-    return nullptr;
+    return 1;
   }
   size_t pos = 0;
   for (size_t i = 0; str[i]; i++)
   {
-    int count = 0;
-    for (size_t j = 0; str[j]; j++)
+    bool already_added = false;
+    for (size_t k = 0; k < pos; k++)
     {
-      if (str[i] == str[j])
+      if (buffer[k] == str[i])
       {
-        count++;
+        already_added = true;
+        break;
       }
     }
+    if (already_added)
+    {
+      continue;
+    }
+    int count = 0;
     if (count > 1)
     {
-      bool already_added = false;
-      for (size_t k = 0; k < pos; k++)
-      {
-        if (buffer[k] == str[i])
-        {
-          already_added = true;
-          break;
-        }
-      }
-      if (!already_added)
-      {
-        buffer[pos++] = str[i];
-      }
+      buffer[pos++] = str[i];
     }
   }
   buffer[pos] = '\0';
-  return buffer;
+  return 0;
 }
 
-char* velizade::uni_two(const char* str1, const char* str2)
+int velizade::uni_two(char* buffer, const char* str1, const char* str2)
 {
-  if (!str1 || !str2)
+  if (!buffer || !str1 || !str2)
   {
-    return nullptr;
-  }
-  size_t len1 = 0;
-  size_t len2 = 0;
-  while (str1[len1])
-  {
-    len1++;
-  }
-  while (str2[len2])
-  {
-    len2++;
-  }
-  size_t total_len = len1 + len2;
-  char* buffer = reinterpret_cast<char*>(malloc(total_len + 1));
-  if (!buffer)
-  {
-    return nullptr;
+    return 1;
   }
   size_t i = 0;
   size_t j = 0;
@@ -150,5 +90,5 @@ char* velizade::uni_two(const char* str1, const char* str2)
     }
   }
   buffer[pos] = '\0';
-  return buffer;
+  return 0;
 }
