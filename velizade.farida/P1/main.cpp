9c83e7c1505fdc0dea3859cad386f52efb9aafab
Farida <velizadefarida@gmail.com>
2026-01-11 11:35:20 +0500

    CG 16

--- velizade.farida/P1/main.cpp
+++ velizade.farida/P1/main.cpp
@@ -9,13 +9,13 @@ int main()
   int val;
   bool got_any = false;
   int rc = 0;
-  for(;;)
+  for (;;)
   {
-    if(!(std::cin >> val))
+    if (!(std::cin >> val))
     {
       break;
     }
-    if(val == 0)
+    if (val == 0)
     {
       break;
     }
@@ -23,15 +23,15 @@ int main()
     lm(val);
     sc(val);
   }
-  if(std::cin.fail())
+  if (std::cin.fail())
   {
-    if(!std::cin.eof())
+    if (!std::cin.eof())
     {
       std::cerr << "bad input" << "\n";
       return 1;
     }
   }
-  if(!got_any)
+  if (!got_any)
   {
     std::cerr << "empty" << "\n";
     return 2;
@@ -40,7 +40,7 @@ int main()
   {
     std::cout << "locmax = " << lm() << "\n";
   }
-  catch(...)
+  catch (...)
   {
     std::cerr << "locmax error" << "\n";
     rc = 2;
@@ -49,7 +49,7 @@ int main()
   {
     std::cout << "sgnchg = " << sc() << "\n";
   }
-  catch(...)
+  catch (...)
   {
     std::cerr << "sgnchg error" << "\n";
     rc = 2;
