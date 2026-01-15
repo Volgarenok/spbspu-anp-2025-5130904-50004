6c1aee7f13f477bc2ab0cc237778533c68654c3d
Ausev.Arteam <seregabest77@gmail.com>
2026-01-15 22:50:38 +0300

    drop old file

--- aushev.arteam/P1/process_sequence.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef AUSHEV_ARTEAM_P1_PROCESS_SEQUENCE_HPP
-#define AUSHEV_ARTEAM_P1_PROCESS_SEQUENCE_HPP
-
-namespace aushev {
-
-bool process_sequence(int& eql_result, int& mon_result);
-}
-#endif
