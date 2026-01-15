568ee485ec8efaafaedb6aff683864d36cfaaa66
emiraydgn <emiraydogan03@gmail.com>
2026-01-15 21:23:29 +0300

    Fixed CG

--- aydogan.emir/P1/main.cpp
+++ aydogan.emir/P1/main.cpp
@@ -5,35 +5,35 @@
 
 int main()
 {
-    int num = 0;
-    aydogan::MonInc mon_inc;
-    aydogan::LocMin loc_min;
+  int num = 0;
+  aydogan::MonInc mon_inc;
+  aydogan::LocMin loc_min;
 
-    size_t count = 0;
+  size_t count = 0;
 
-    while ((std::cin >> num) && (num != 0))
-    {
-        mon_inc(num);
-        loc_min(num);
-        ++count;
-    }
+  while ((std::cin >> num) && (num != 0))
+  {
+    mon_inc(num);
+    loc_min(num);
+    ++count;
+  }
 
-    if (std::cin.fail() && !std::cin.eof())
-    {
-        std::cerr << "Input error\n";
-        return 1;
-    }
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "Input error\n";
+    return 1;
+  }
 
-    try
-    {
-       std::cout << mon_inc() << "\n";
-       std::cout << loc_min() << "\n";
-    }
-    catch (const std::logic_error& e)
-    {
-       std::cerr << e.what() << "\n";
-       return 2;
-    }
+  try
+  {
+    std::cout << mon_inc() << "\n";
+    std::cout << loc_min() << "\n";
+  }
+  catch (const std::logic_error& e)
+  {
+    std::cerr << e.what() << "\n";
+    return 2;
+  }
 
-    return 0;
+  return 0;
 }
--- aydogan.emir/P1/mon_inc.cpp
+++ aydogan.emir/P1/mon_inc.cpp
@@ -2,31 +2,31 @@
 #include <limits>
 
 aydogan::MonInc::MonInc()
-    : max_len_(0),
-      cur_len_(0),
-      prev_(std::numeric_limits< int >::max())
+  : max_len_(0),
+    cur_len_(0),
+    prev_(std::numeric_limits< int >::max())
 {}
 
 void aydogan::MonInc::operator()(int n)
 {
-    if (n > prev_)
-    {
-        ++cur_len_;
-    }
-    else
-    {
-        cur_len_ = 1;
-    }
+  if (n > prev_)
+  {
+    ++cur_len_;
+  }
+  else
+  {
+    cur_len_ = 1;
+  }
 
-    if (cur_len_ > max_len_)
-    {
-        max_len_ = cur_len_;
-    }
+  if (cur_len_ > max_len_)
+  {
+    max_len_ = cur_len_;
+  }
 
-    prev_ = n;
+  prev_ = n;
 }
 
 size_t aydogan::MonInc::operator()() const
 {
-    return max_len_;
+  return max_len_;
 }
