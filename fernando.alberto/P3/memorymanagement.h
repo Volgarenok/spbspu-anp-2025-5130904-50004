f20d2db4e89e157bb24eef05dcc88adf51e8c6bb
Альберту Фернанду <fernandodiasfd780518@gmail.com>
2026-01-16 17:45:13 -0300

    Update memorymanagement.h

--- fernando.alberto/P3/memorymanagement.h
+++ fernando.alberto/P3/memorymanagement.h
@@ -1,9 +1,10 @@
 #ifndef MEMORY_MANAGEMENT_H
 #define MEMORY_MANAGEMENT_H
 
-namespace fernando {
-    int* allocate_matrix(int rows, int cols);
-    void free_matrix(int* matrix);
+namespace fernando
+{
+  int* allocate_matrix(int rows, int cols);
+  void free_matrix(int* matrix);
 }
 
 #endif
