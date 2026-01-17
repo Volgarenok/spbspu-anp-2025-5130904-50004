8c0f1e3c89436026d907e2f5d3e3134f2d2a73cb
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-01-17 16:15:21 -0300

    initialize commit

---
+++ fernando.alberto/P1/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "EqualSequence.hpp"
+#include "MonotonicIncreasing.hpp"
+
+int main()
+{
+  int num = 0;
+  alberto::EqualSequence count_equal;
+  alberto::MonotonicIncreasing count_monotonic;
+  std::size_t element_count = 0;
+
+  while (std::cin >> num && num != 0)
+  {
+    if (std::cin.fail() && !std::cin.eof())
+    {
+      std::cerr << "Error: invalid input." << "\n";
+      return 1;
+    }
+
+    count_equal(num);
+    count_monotonic(num);
+    ++element_count;
+  }
+
+  if (std::cin.fail() && !std::cin.eof())
+  {
+    std::cerr << "Error: invalid input." << "\n";
+    return 1;
+  }
+
+  if (element_count == 0)
+  {
+    std::cout << 0 << "\n" << 0 << "\n";
+    return 0;
+  }
+
+  std::cout << count_equal() << "\n" << count_monotonic() << "\n";
+  return 0;
+}
