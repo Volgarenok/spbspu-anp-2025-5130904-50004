982a90834b9a3bf462976ccb629d8e7096d8e684
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-01-19 01:26:23 +0300

    realize  evn-cnt

--- dolenko.aleksandr/P1/functions.cpp
+++ dolenko.aleksandr/P1/functions.cpp
@@ -26,4 +26,33 @@ int count_divisible_by_previous(const int* seq, size_t count, bool& can_calculat
   return result;
 }
 
+int max_consecutive_even(const int* seq, size_t count)
+{
+  if (count == 0)
+  {
+    return 0;
+  }
+
+  int max_consecutive = 0;
+  int current_consecutive = 0;
+
+  for (size_t i = 0; i < count; ++i)
+  {
+    if (seq[i] % 2 == 0)
+    {
+      current_consecutive++;
+      if (current_consecutive > max_consecutive)
+      {
+        max_consecutive = current_consecutive;
+      }
+    }
+    else
+    {
+      current_consecutive = 0;
+    }
+  }
+
+  return max_consecutive;
+}
+
 }
