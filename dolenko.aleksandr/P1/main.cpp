760d4948a71866e174d8d17a02ee7b22bbe39fe8
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-01-19 01:34:00 +0300

    Add sequence reading loop

--- dolenko.aleksandr/P1/main.cpp
+++ dolenko.aleksandr/P1/main.cpp
@@ -5,5 +5,27 @@
 
 int main()
 {
+  constexpr size_t max_sequence_size = 1000;
+  int sequence[max_sequence_size];
+  size_t count = 0;
+
+  int value = 0;
+  while (std::cin >> value)
+  {
+    if (value == 0)
+    {
+      break;
+    }
+
+    if (count >= max_sequence_size)
+    {
+      std::cerr << "Sequence too long" << "\n";
+      return 2;
+    }
+
+    sequence[count] = value;
+    count++;
+  }
+
   return 0;
 }
