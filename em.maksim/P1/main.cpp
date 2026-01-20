f5dd1d023fd4d870508bc1f75a61a52229b29609
Emich-M <fagetolrer@gmail.com>
2026-01-20 12:46:46 +0300

    Added Code Validation

--- em.maksim/P1/main.cpp
+++ em.maksim/P1/main.cpp
@@ -2,15 +2,40 @@
 #include "evn_cnt.hpp"
 #include <iostream>
 
+namespace em {
+
+static bool is_valid_sequence() {
+    int x;
+    while (std::cin >> x) {
+        if (x == 0) return true;
+    }
+    return false;
+}
+
+}
+
 int main() {
-    em::IncSeq inc_seq;
-    em::EvnCnt evn_cnt;
+    using namespace em;
+
+    if (!is_valid_sequence()) {
+        std::cerr << "Invalid input sequence\n";
+        return 1;
+    }
+
+    std::cin.clear();
+    std::cin.seekg(0);
+
+    IncSeq inc_seq;
+    EvnCnt evn_cnt;
+
     int x;
     while (std::cin >> x && x != 0) {
         inc_seq(x);
         evn_cnt(x);
     }
+
     std::cout << inc_seq() << '\n';
     std::cout << evn_cnt() << '\n';
+
     return 0;
 }
