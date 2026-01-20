5ccf5c8b5ebee8f79c20c031f4b7f43ecd391d90
maxchadin <maxchadin080418@gmail.com>
2026-01-20 17:12:09 +0300

    fix matrix_actions.hpp

--- chadin.maxim/P3/matrix_actions.hpp
+++ chadin.maxim/P3/matrix_actions.hpp
@@ -1,14 +1,15 @@
 #ifndef MATRIX_ACTIONS_HPP
 #define MATRIX_ACTIONS_HPP
+
 #include <iostream>
 #include <cstddef>
 
 namespace chadin
 {
-std::istream& readArr(std::istream& input, int* arr, std::size_t rows, std::size_t cols);
-std::ostream& writeArr(std::ostream& output, const int* arr, std::size_t rows, std::size_t cols, int result);
-void increaseElements(int* arr, std::size_t rows, std::size_t cols);
-int countDiagonals(const int* arr, std::size_t rows, std::size_t cols);
+  std::istream& readArr(std::istream& input, int* arr, size_t rows, size_t cols);
+  std::ostream& writeArr(std::ostream& output, const int* arr, size_t rows, size_t cols);
+  void increaseElements(int* arr, size_t rows, size_t cols);
+  int countDiagonals(const int* arr, size_t rows, size_t cols);
 }
 
 #endif
