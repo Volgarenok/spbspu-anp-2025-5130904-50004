cc55b062884a29fe7053fc61fe831cb81b8fcc03
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-01-22 19:47:14 +0300

    add skeleton dif_lat

---
+++ dolenko.aleksandr/P4/dif_lat.cpp
@@ -0,0 +1,19 @@
+#include "strings.hpp"
+#include <cctype>
+
+namespace dolenko
+{
+
+size_t dif_lat(const char* arr)
+{
+  size_t count = 0;
+  size_t ind = 0;
+  char symb = '\0';
+  const size_t alp = 26;
+  bool info[alp] = {};
+
+
+  return count;
+}
+
+} 
