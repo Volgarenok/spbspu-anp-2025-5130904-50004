b306b2548895ca5330391477ec970ccf027d4db0
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-01-22 19:58:19 +0300

    realise uni_two

--- dolenko.aleksandr/P4/uni_two.cpp
+++ dolenko.aleksandr/P4/uni_two.cpp
@@ -14,6 +14,19 @@ char* uni_two(char* dest, const char* str1, const char* str2)
   size_t j = 0;
   size_t index = 0;
 
+  while (str1[i] != '\0' || str2[j] != '\0')
+  {
+    if (str1[i] != '\0')
+    {
+      dest[index++] = str1[i++];
+    }
+    if (str2[j] != '\0')
+    {
+      dest[index++] = str2[j++];
+    }
+  }
+
+  dest[index] = '\0';
   return dest;
 }
 
