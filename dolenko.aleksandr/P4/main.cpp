e58ae6133df5a74f8edbc748cfc09030f9daeaae
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-01-22 19:03:13 +0300

    check for empty input

--- dolenko.aleksandr/P4/main.cpp
+++ dolenko.aleksandr/P4/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-#include <cstring>
-
-using namespace std;
+#include "strings.hpp"
 
 int main()
 {
+  char* str1 = nullptr;
+  char* str_unified = nullptr;
+  const char* str2 = "def_";
+
+  try
+  {
+    str1 = dolenko::getline(std::cin, str1);
+  }
+  catch (const std::bad_alloc& error)
+  {
+    std::cerr << error.what();
+    return 1;
+  }
+
+  if (!str1 || *str1 == '\0')
+  {
+    std::cerr << "Missing string\n";
+    delete[] str1;
+    return 1;
+  }
+
+
   return 0;
 }
