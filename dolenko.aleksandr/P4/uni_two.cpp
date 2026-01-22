c5b8d20be386500f8d2a3151ed0c3b919d60ec71
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-01-22 19:57:20 +0300

    Add uni_two skeleton

---
+++ dolenko.aleksandr/P4/uni_two.cpp
@@ -0,0 +1,20 @@
+#include "strings.hpp"
+
+namespace dolenko
+{
+
+char* uni_two(char* dest, const char* str1, const char* str2)
+{
+  if (!dest || !str1 || !str2)
+  {
+    return dest;
+  }
+
+  size_t i = 0;
+  size_t j = 0;
+  size_t index = 0;
+
+  return dest;
+}
+
+} 
